6f3b2e2a578a21a4cd1988167774c2dad29ac6e3
Jianting Zhang <zjtnssl@yahoo.com>
2019-08-23 07:48:37 -0700

    Update cpp/include/cuspatial/coord_trans.hpp
    Co-Authored-By: Mark Harris <mharris@nvidia.com>

--- cpp/include/cuspatial/coord_trans.hpp
+++ cpp/include/cuspatial/coord_trans.hpp
@@ -11,7 +11,7 @@ namespace cuSpatial {
  * @param[in] cam_lat: latitude of camera origin
  * @param[in] in_lon: array of longitude coordinates of input points to be transformed
  * @param[in] in_lat: array of latitude coordinates of input points to be transformed
- * @param[out] out_x: array of x coordiantes after transformation (in kilometers -km)
+ * @param[out] out_x: column of x coordinates after transformation in kilometers (km)
  * @param[out] out_y: array of y coordiantes after transformation (in kilometers -km)
  */
 void lonlat_to_coord(const gdf_scalar  & cam_lon,const gdf_scalar  & cam_lat,const gdf_column  & in_lon,const gdf_column  & in_lat,
