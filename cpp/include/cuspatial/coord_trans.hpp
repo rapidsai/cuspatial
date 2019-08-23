67f3356b60547e7f841b0485eb4c78d377184e96
Jianting Zhang <zjtnssl@yahoo.com>
2019-08-23 07:48:57 -0700

    Update cpp/include/cuspatial/coord_trans.hpp
    Co-Authored-By: Mark Harris <mharris@nvidia.com>

--- cpp/include/cuspatial/coord_trans.hpp
+++ cpp/include/cuspatial/coord_trans.hpp
@@ -12,7 +12,7 @@ namespace cuSpatial {
  * @param[in] in_lon: array of longitude coordinates of input points to be transformed
  * @param[in] in_lat: array of latitude coordinates of input points to be transformed
  * @param[out] out_x: column of x coordinates after transformation in kilometers (km)
- * @param[out] out_y: array of y coordiantes after transformation (in kilometers -km)
+ * @param[out] out_y: column of y coordinates after transformation in kilometers (km)
  */
 void lonlat_to_coord(const gdf_scalar  & cam_lon,const gdf_scalar  & cam_lat,const gdf_column  & in_lon,const gdf_column  & in_lat,
     	gdf_column & out_x,gdf_column & out_y /* ,cudaStream_t stream = 0   */);
