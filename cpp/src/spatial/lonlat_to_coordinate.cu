d52a80bb00da168a517cb03276782261aaaccf65
zhangjianting <zjtnssl@yahoo.com>
2019-09-03 14:46:58 -0400

    code clean-up: use anonymous namespace, remove output and timing code

--- cpp/src/spatial/lonlat_to_coordinate.cu
+++ cpp/src/spatial/lonlat_to_coordinate.cu
@@ -19,11 +19,15 @@
 #include <type_traits>
 #include <utility>
 #include <thrust/device_vector.h>
-#include <sys/time.h>
-#include <time.h>
 
 #include <utility/utility.hpp>
 #include <cuspatial/coordinate_transform.hpp>
+
+
+namespace {
+
+//A port from https://gitlab-master.nvidia.com/metromind/DS-ITS-app/stream-ITS/blob/master/src/main/scala/com/nvidia/ds/its/util/Util.scala
+//Note: points in the third quadrant relative camera will be translated into points in the first quadrant - use with caution
 
 template <typename T>
 __global__ void coord_trans_kernel(gdf_size_type loc_size,
@@ -41,80 +45,56 @@ __global__ void coord_trans_kernel(gdf_size_type loc_size,
 }
 
 struct ll2coord_functor {
-    template <typename col_type>
+    template <typename T>
     static constexpr bool is_supported()
     {
-        return std::is_floating_point<col_type>::value;
+        return std::is_floating_point<T>::value;
     }
 
-    template <typename col_type, std::enable_if_t< is_supported<col_type>() >* = nullptr>
+    template <typename T, std::enable_if_t< is_supported<T>() >* = nullptr>
     std::pair<gdf_column,gdf_column> operator()(const gdf_scalar  & cam_lon,const gdf_scalar  & cam_lat,
     	 const gdf_column  & in_lon,const gdf_column  & in_lat)
     	
     {
         gdf_column  out_x, out_y;
-        int num_print=(in_lon.size<10)?in_lon.size:10;
-        std::cout<<"ll2coord: showing the first "<< num_print<<" output records"<<std::endl;
-        std::cout<<"in_lon"<<std::endl;
-        thrust::device_ptr<col_type> in_lon_ptr=thrust::device_pointer_cast(static_cast<col_type*>(in_lon.data));
-        thrust::copy(in_lon_ptr,in_lon_ptr+10,std::ostream_iterator<col_type>(std::cout, " "));std::cout<<std::endl; 
-        std::cout<<"in_lat"<<std::endl;
-        thrust::device_ptr<col_type>in_lat_ptr=thrust::device_pointer_cast(static_cast<col_type*>(in_lat.data));
-        thrust::copy(in_lat_ptr,in_lat_ptr+10,std::ostream_iterator<col_type>(std::cout, " "));std::cout<<std::endl;  
- 
- 	out_x.dtype= in_lon.dtype;
-  	out_x.col_name=(char *)malloc(strlen("x")+ 1);
-	strcpy(out_x.col_name,"x");    
-        RMM_TRY( RMM_ALLOC(&out_x.data, in_lon.size * sizeof(col_type), 0) );
-     	out_x.size=in_lon.size;
-     	out_x.valid=nullptr;
-     	out_x.null_count=0;		
+        memset(&out_x,0,sizeof(gdf_column));
+        memset(&out_y,0,sizeof(gdf_column));
+        
+        cudaStream_t stream{0};
+        T* temp_x{nullptr};
+        T* temp_y{nullptr};
+        RMM_TRY( RMM_ALLOC(&temp_x, in_lon.size * sizeof(T), stream) );
+        RMM_TRY( RMM_ALLOC(&temp_y, in_lat.size * sizeof(T), stream) );
 
- 	out_y.dtype= in_lat.dtype;
-  	out_y.col_name=(char *)malloc(strlen("y")+ 1);
-	strcpy(out_x.col_name,"x");    
-        RMM_TRY( RMM_ALLOC(&out_y.data, in_lon.size * sizeof(col_type), 0) );
-     	out_y.size=in_lat.size;
-     	out_y.valid=nullptr;
-     	out_y.null_count=0;	
-        
-        struct timeval t0,t1;
-        gettimeofday(&t0, nullptr);
-        
+        gdf_column_view_augmented(&out_x, temp_x, nullptr, in_lon.size,
+                                in_lon.dtype, 0,
+                                gdf_dtype_extra_info{TIME_UNIT_NONE}, "x");          
+        gdf_column_view_augmented(&out_y, temp_y, nullptr, in_lat.size,
+                                in_lat.dtype, 0,
+                                gdf_dtype_extra_info{TIME_UNIT_NONE}, "y");          
+
         gdf_size_type min_grid_size = 0, block_size = 0;
-        CUDA_TRY( cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size, coord_trans_kernel<col_type>) );
+        CUDA_TRY( cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size, coord_trans_kernel<T>) );
         cudf::util::cuda::grid_config_1d grid{in_lon.size, block_size, 1};
-        std::cout<<"in_lon.size="<<in_lon.size<<" block_size="<<block_size<<std::endl;
        
-        coord_trans_kernel<col_type> <<< grid.num_blocks, block_size >>> (in_lon.size,
+        coord_trans_kernel<T> <<< grid.num_blocks, block_size >>> (in_lon.size,
         	*((double*)(&(cam_lon.data))),*((double*)(&(cam_lat.data))),
-   	    	static_cast<col_type*>(in_lon.data), static_cast<col_type*>(in_lat.data),
-   	    	static_cast<col_type*>(out_x.data), static_cast<col_type*>(out_y.data) );           
+   	    	static_cast<T*>(in_lon.data), static_cast<T*>(in_lat.data),
+   	    	temp_x, temp_y );           
         CUDA_TRY( cudaDeviceSynchronize() );
-
-        gettimeofday(&t1, nullptr);
-        float ll2coord_kernel_time = cuspatial::calc_time("lon/lat to x/y conversion kernel time in ms=",t0,t1);
-
-        num_print=(out_x.size<10)?out_x.size:10;
-        std::cout<<"ll2coord: showing the first "<< num_print<<" output records"<<std::endl;
-        thrust::device_ptr<col_type> outx_ptr=thrust::device_pointer_cast(static_cast<col_type*>(out_x.data));
-        thrust::device_ptr<col_type> outy_ptr=thrust::device_pointer_cast(static_cast<col_type*>(out_y.data));
-        std::cout<<"out_x"<<std::endl;     
-        thrust::copy(outx_ptr,outx_ptr+num_print,std::ostream_iterator<col_type>(std::cout, " "));std::cout<<std::endl;     
-        std::cout<<"out_y"<<std::endl;     
- 	thrust::copy(outy_ptr,outy_ptr+num_print,std::ostream_iterator<col_type>(std::cout, " "));std::cout<<std::endl;  
- 	
+	
  	return std::make_pair(out_x,out_y);
     }
 
-    template <typename col_type, std::enable_if_t< !is_supported<col_type>() >* = nullptr>
+    template <typename T, std::enable_if_t< !is_supported<T>() >* = nullptr>
     std::pair<gdf_column,gdf_column> operator()(const gdf_scalar  & cam_lon,const gdf_scalar  & cam_lat,
     	const gdf_column  & in_lon,const gdf_column  & in_lat)
     {
         CUDF_FAIL("Non-floating point operation is not supported");
     }
 };
-    
+
+} // namespace anonymous        
 
 namespace cuspatial {
 
@@ -126,10 +106,8 @@ namespace cuspatial {
 std::pair<gdf_column,gdf_column> lonlat_to_coord(const gdf_scalar& cam_lon, const gdf_scalar& cam_lat,
 	const gdf_column& in_lon, const gdf_column  & in_lat)
 
-{       
-    struct timeval t0,t1;
-    gettimeofday(&t0, nullptr);
-    
+{           
+
     double cx=*((double*)(&(cam_lon.data)));
     double cy=*((double*)(&(cam_lat.data)));
     CUDF_EXPECTS(cx >=-180 && cx <=180 && cy >=-90 && cy <=90,
@@ -142,11 +120,8 @@ std::pair<gdf_column,gdf_column> lonlat_to_coord(const gdf_scalar& cam_lon, cons
     
     auto res=cudf::type_dispatcher(in_lon.dtype, ll2coord_functor(), cam_lon,cam_lat,in_lon,in_lat);
     
-    // handle null_count if needed 
-     
-    gettimeofday(&t1, nullptr);
-    float ll2coord_end2end_time=calc_time("lon/lat to x/y conversion end2end time in ms=",t0,t1);
     return res;
+    
   }//lonlat_to_coord 
     	
 }// namespace cuspatial
