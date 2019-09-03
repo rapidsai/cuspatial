3f5a931dedfec3c1cb0481bddf9443e78daf4f20
Mark Harris <mharris@nvidia.com>
2019-09-02 22:33:27 -0700

    Clean up polygon_soa_reader

--- cpp/src/io/soa/polygon_soa_reader.cu
+++ cpp/src/io/soa/polygon_soa_reader.cu
@@ -20,83 +20,80 @@
 #include <cuda_runtime.h>
 #include <thrust/device_vector.h>
 #include <thrust/device_ptr.h>
-#include <rmm/rmm.h>
+#include <rmm/thrust_rmm_allocator.h>
 #include <cudf/types.h>
+#include <cudf/legacy/column.hpp>
 #include <utilities/error_utils.hpp>
 #include <cuspatial/soa_readers.hpp>
 #include <utility/utility.hpp>
 
 namespace cuspatial
 {
-	/**
-	 * @brief read poygon data from file in SoA format; data type of vertices is fixed to double (GDF_FLOAT64)
-	 * see soa_readers.hpp
-	*/	
-	
-	void read_polygon_soa(const char *filename,gdf_column* ply_fpos, gdf_column* ply_rpos,
-		gdf_column* ply_x,gdf_column* ply_y)
-	{
-	        CUDF_EXPECTS(ply_fpos!=nullptr && ply_rpos!=nullptr && ply_x!=nullptr && ply_y!=nullptr,
-	        	"none of the polygon offset/ring offset/x coorinate/y coordinate column can be null ");
-	        
-	        memset(ply_fpos,0,sizeof(gdf_column));
-	        memset(ply_rpos,0,sizeof(gdf_column));
-	        memset(ply_x,0,sizeof(gdf_column));
-	        memset(ply_y,0,sizeof(gdf_column));
-	
-	        struct polygons<double> pm;
-	        read_polygon_soa<double>(filename,&pm);	        
-	        if(pm.num_feature<=0) return;
-	        
-  		ply_fpos->dtype=GDF_INT32;
-  		ply_fpos->col_name=(char *)malloc(strlen("f_pos")+ 1);
-		strcpy(ply_fpos->col_name,"f_pos");
-		ply_fpos->data=nullptr;
-		RMM_TRY( RMM_ALLOC(&(ply_fpos->data), pm.num_feature * sizeof(uint32_t), 0) );
-		cudaMemcpy(ply_fpos->data, pm.feature_length,pm.num_feature * sizeof(uint32_t) , cudaMemcpyHostToDevice);
-		thrust::device_ptr<uint32_t> d_pfp_ptr=thrust::device_pointer_cast((uint32_t *)(ply_fpos->data));
-		//prefix-sum: len to pos
-		thrust::inclusive_scan(d_pfp_ptr,d_pfp_ptr+pm.num_feature,d_pfp_ptr);
-		ply_fpos->size=pm.num_feature;
-		ply_fpos->valid=nullptr;
-		ply_fpos->null_count=0;
-		delete[] pm.feature_length;
+/*
+    * read polygon data from file in SoA format; data type of vertices is fixed to double (GDF_FLOAT64)
+    * see soa_readers.hpp
+*/
+void read_polygon_soa(const char *filename,
+                      gdf_column* ply_fpos, gdf_column* ply_rpos,
+                      gdf_column* ply_x, gdf_column* ply_y)
+{
+    CUDF_EXPECTS(ply_fpos != nullptr && ply_rpos != nullptr &&
+                 ply_x != nullptr && ply_y != nullptr,
+                 "Null column pointer");
 
- 		ply_rpos->dtype=GDF_INT32;
- 		ply_rpos->col_name=(char *)malloc(strlen("r_pos")+ 1);
-		strcpy(ply_rpos->col_name,"r_pos");
-		ply_rpos->data=nullptr;
-		RMM_TRY( RMM_ALLOC(&(ply_rpos->data), pm.num_ring * sizeof(uint32_t), 0) );
-		cudaMemcpy(ply_rpos->data, pm.ring_length,pm.num_ring * sizeof(uint32_t) , cudaMemcpyHostToDevice);
-		thrust::device_ptr<uint32_t> d_prp_ptr=thrust::device_pointer_cast((uint32_t *)(ply_rpos->data));
-		//prefix-sum: len to pos
-		thrust::inclusive_scan(d_prp_ptr,d_prp_ptr+pm.num_ring,d_prp_ptr);
-		ply_rpos->size=pm.num_ring;
-		ply_rpos->valid=nullptr;
-		ply_rpos->null_count=0;
-		delete[] pm.ring_length;
+    memset(ply_fpos,0,sizeof(gdf_column));
+    memset(ply_rpos,0,sizeof(gdf_column));
+    memset(ply_x,0,sizeof(gdf_column));
+    memset(ply_y,0,sizeof(gdf_column));
 
- 		ply_x->dtype= GDF_FLOAT64;
- 		ply_x->col_name=(char *)malloc(strlen("x")+ 1);
-		strcpy(ply_x->col_name,"x");
-		RMM_TRY( RMM_ALLOC(&(ply_x->data), pm.num_vertex * sizeof(double), 0) );
-		cudaMemcpy(ply_x->data, pm.x,pm.num_vertex * sizeof(double) , cudaMemcpyHostToDevice);		
-		ply_x->size=pm.num_vertex;
-		ply_x->valid=nullptr;
-		ply_x->null_count=0;		
-		delete[] pm.x;
+    struct polygons<double> pm;
+    read_polygon_soa<double>(filename, &pm);
+    if (pm.num_feature <=0) return;
 
- 		ply_y->dtype= GDF_FLOAT64;
- 		ply_y->col_name=(char *)malloc(strlen("y")+ 1);
-		strcpy(ply_y->col_name,"y");
-		ply_y->data=nullptr;
-		RMM_TRY( RMM_ALLOC(&(ply_y->data), pm.num_vertex * sizeof(double), 0) );
-		cudaMemcpy(ply_y->data, pm.y,pm.num_vertex * sizeof(double) , cudaMemcpyHostToDevice);		
-		ply_y->size=pm.num_vertex;
-		ply_y->valid=nullptr;
-		ply_y->null_count=0;
-		delete[] pm.y;
-		
-		delete[] pm.group_length;
-	}//read_polygon_soa
+    cudaStream_t stream{0};
+    auto exec_policy = rmm::exec_policy(stream)->on(stream);
+
+    int32_t* temp{nullptr};
+    RMM_TRY( RMM_ALLOC(&temp, pm.num_feature * sizeof(int32_t), stream) );
+    CUDA_TRY( cudaMemcpyAsync(temp, pm.feature_length,
+                              pm.num_feature * sizeof(int32_t),
+                              cudaMemcpyHostToDevice, stream) );
+    //prefix-sum: len to pos
+    thrust::inclusive_scan(exec_policy, temp, temp + pm.num_feature, temp);
+    gdf_column_view_augmented(ply_fpos, temp, nullptr, pm.num_feature,
+                              GDF_INT32, 0,
+                              gdf_dtype_extra_info{TIME_UNIT_NONE}, "f_pos");
+
+    RMM_TRY( RMM_ALLOC(&temp, pm.num_ring * sizeof(int32_t), stream) );
+    CUDA_TRY( cudaMemcpyAsync(temp, pm.ring_length,
+                              pm.num_ring * sizeof(int32_t),
+                              cudaMemcpyHostToDevice, stream) );
+    thrust::inclusive_scan(exec_policy, temp, temp + pm.num_feature, temp);
+    gdf_column_view_augmented(ply_rpos, temp, nullptr, pm.num_ring,
+                              GDF_INT32, 0,
+                              gdf_dtype_extra_info{TIME_UNIT_NONE}, "r_pos");
+
+    RMM_TRY( RMM_ALLOC(&temp, pm.num_vertex * sizeof(double), stream) );
+    CUDA_TRY( cudaMemcpyAsync(temp, pm.x,
+                              pm.num_vertex * sizeof(double),
+                              cudaMemcpyHostToDevice, stream) );
+    gdf_column_view_augmented(ply_x, temp, nullptr, pm.num_vertex,
+                              GDF_FLOAT64, 0,
+                              gdf_dtype_extra_info{TIME_UNIT_NONE}, "x");
+
+    RMM_TRY( RMM_ALLOC(&temp, pm.num_vertex * sizeof(double), stream) );
+    CUDA_TRY( cudaMemcpyAsync(temp, pm.y,
+                              pm.num_vertex * sizeof(double),
+                              cudaMemcpyHostToDevice, stream) );
+    gdf_column_view_augmented(ply_y, temp, nullptr, pm.num_vertex,
+                              GDF_FLOAT64, 0,
+                              gdf_dtype_extra_info{TIME_UNIT_NONE}, "y");
+
+    delete[] pm.feature_length;
+    delete[] pm.ring_length;
+    delete[] pm.x;
+    delete[] pm.y;
+    delete[] pm.group_length;
+}
+
 }// namespace cuspatial
