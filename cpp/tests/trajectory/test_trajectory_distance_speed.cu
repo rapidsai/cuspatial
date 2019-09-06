c4e8484e78ebb8c8f05e3b3667b9e511869c425d
Mark Harris <mharris@nvidia.com>
2019-09-05 23:10:11 -0700

    distance_speed test improvements

--- cpp/tests/trajectory/test_trajectory_distance_speed.cu
+++ cpp/tests/trajectory/test_trajectory_distance_speed.cu
@@ -32,61 +32,104 @@
 #include <tests/utilities/cudf_test_utils.cuh>
 #include <tests/utilities/cudf_test_fixtures.h>
 
+MATCHER_P(FloatNearPointwise, tol, "Out of range") {
+    return (std::get<0>(arg) > std::get<1>(arg) - tol &&
+            std::get<0>(arg) < std::get<1>(arg) + tol) ;
+}
+
 struct TrajectoryDistanceSpeed : public GdfTest 
 {
-};   
-   
-TEST_F(TrajectoryDistanceSpeed, DistanceAndSpeed)
+};
+
+template <typename T>
+using wrapper = cudf::test::column_wrapper<T>;
+
+constexpr gdf_size_type column_size{100};
+
+constexpr int m_per_km = 1000;
+constexpr int ms_per_second = 1000;
+constexpr int ms_per_hour = ms_per_second * 60 * 60;
+
+TEST_F(TrajectoryDistanceSpeed, DistanceAndSpeedThree)
 {
-    //three sorted trajectories with 5,4,3 points, respectively
-    std::cout<<"in TrajectoryDeriveToy"<<std::endl;
-    //assuming x/y are in the unit of killometers (km); 
-    //computed distance and speed are in the units of meters and m/s, respectively
-    double point_x[]={1.0,2.0,3.0,5.0,7.0,1.0,2.0,3.0,6.0,0.0,3.0,6.0};
-    double point_y[]={0.0,1.0,2.0,3.0,1.0,3.0,5.0,6.0,5.0,4.0,7.0,4.0};
-    uint32_t traj_len[]={5,4,3};
-    uint32_t traj_offset[]={5,9,12};
+    std::vector<int32_t> sequence(column_size);
+    std::iota(sequence.begin(), sequence.end(), 0);
 
-    //handling timestamps - use millsecond field only for now 
-    int point_hh[]={0,1,2,3,4,0,1,2,3,0,1,2};
-    int point_ms[]={1,2,3,4,5,1,2,3,4,1,2,3};
-    int num_point=sizeof(point_x)/sizeof(double);
-    int num_traj=sizeof(traj_len)/sizeof(uint32_t);
-    std::cout<<"num_point="<<num_point<<"   num_traj="<<num_traj<<std::endl;
+    //three sorted trajectories: one with 2/3 of the points, two with 1/6
+    std::vector<int32_t> id_vector(column_size);
+    std::transform(sequence.cbegin(), sequence.cend(), id_vector.begin(),
+                   [](int32_t i) { 
+                       return (i < 2 * column_size / 3) ? 0 : 
+                              (i < 5 * column_size / 6) ? 1 : 2; 
+                    });
 
-    std::vector<cuspatial::its_timestamp> point_ts;
-    for(int i=0;i<num_point;i++)
-    {
-        cuspatial::its_timestamp ts;
-        memset(&ts,0,sizeof(cuspatial::its_timestamp));
-        ts.hh=point_hh[i];
-        ts.ms=point_ms[i];	
-        point_ts.push_back(ts);
+    // timestamp milliseconds
+    std::vector<int64_t> ms_vector(column_size);
+    std::transform(sequence.cbegin(), sequence.cend(), ms_vector.begin(), 
+                   [](int32_t i) { return i * ms_per_hour + i; }); 
+
+    std::vector<double> x(sequence.begin(), sequence.end());
+    wrapper<double> in_x{x};
+        //[&](gdf_index_type i) { return static_cast<double>(sequence[i]); });
+    wrapper<double> in_y(column_size,
+        [&](gdf_index_type i) { return static_cast<double>(sequence[i]); });
+    wrapper<cudf::timestamp> in_ts(column_size,
+        [&](gdf_index_type i) {
+            return static_cast<cudf::timestamp>(ms_vector[sequence[i]]);
+        });
+
+    std::vector<gdf_size_type> trajectory_length{2 * column_size / 3, 
+                                                 (column_size + 5) / 6,
+                                                 (column_size + 5) / 6};
+    std::vector<gdf_size_type> trajectory_offset{2 * column_size / 3,
+                                                 5 * column_size / 6,
+                                                 column_size};
+    wrapper<gdf_size_type> traj_len(trajectory_length);
+    wrapper<gdf_size_type> traj_offset{trajectory_offset};
+
+    std::pair<gdf_column, gdf_column> distance_speed;
+
+    EXPECT_NO_THROW(
+     distance_speed = cuspatial::trajectory_distance_and_speed(in_x, in_y,
+                                                               in_ts, traj_len,
+                                                               traj_offset);
+    );
+
+    EXPECT_EQ(distance_speed.first.size, 3);
+    EXPECT_EQ(distance_speed.second.size, 3);
+
+    std::vector<gdf_size_type> id{0, 1, 2};
+    std::vector<double> distance(3, 0);
+    std::vector<double> time(3, 0);
+    std::vector<double> speed(3, 0);
+    
+    // compute expected distance and speed
+    for (auto i : id) {
+        gdf_size_type length = trajectory_length[i];
+        gdf_size_type begin = (i == 0) ? 0 : trajectory_offset[i - 1];
+        gdf_size_type end = trajectory_offset[i];
+        EXPECT_EQ(end, begin + length);
+
+        for (gdf_size_type j = 0; j < length-1; j++) {
+            double d = static_cast<double>(sequence[begin + j + 1] -
+                                           sequence[begin + j]);
+            double ms = static_cast<double>(ms_vector[begin + j + 1] -
+                                            ms_vector[begin + j]);
+            d = sqrt(2 * d * d); // since x == y
+            distance[i] += d * m_per_km;
+            time[i] += (ms / ms_per_second);
+        }
+        speed[i] = distance[i] / time[i];
     }
 
-    cudf::test::column_wrapper<double> point_x_wrapp{std::vector<double>(point_x,point_x+num_point)};
-    cudf::test::column_wrapper<double> point_y_wrapp{std::vector<double>(point_y,point_y+num_point)};
-    cudf::test::column_wrapper<cuspatial::its_timestamp> point_ts_wrapp{point_ts};
-    cudf::test::column_wrapper<uint32_t> traj_len_wrapp{std::vector<uint32_t>(traj_len,traj_len+num_traj)};
-    cudf::test::column_wrapper<uint32_t> traj_pos_wrapp{std::vector<uint32_t>(traj_offset,traj_offset+num_traj)};
-
-    std::cout<<"calling cuspatial::trajectory_distance_and_speed"<<std::endl;
-    std::pair<gdf_column,gdf_column> dist_speed_pair=
-    cuspatial::trajectory_distance_and_speed(*(point_x_wrapp.get()),*(point_y_wrapp.get()),
-    *(point_ts_wrapp.get()),*(traj_len_wrapp.get()),*(traj_pos_wrapp.get()));
-
-    CUDF_EXPECTS(num_traj==dist_speed_pair.first.size && num_traj==dist_speed_pair.second.size,
-        "size of output dist/speed columns should be the same as the number of the input trajectories");
-
-    std::cout<<"computed distance/speed"<<std::endl;
-    int num_print = (num_traj<10)?num_traj:10;  
-    thrust::device_ptr<double> traj_distance_ptr=
-        thrust::device_pointer_cast(static_cast<double*>(dist_speed_pair.first.data));
-    thrust::device_ptr<double> traj_speed_ptr=
-        thrust::device_pointer_cast(static_cast<double*>(dist_speed_pair.second.data));
-
-    std::cout<<"computed distance (in meters):"<<std::endl;
-    thrust::copy(traj_distance_ptr,traj_distance_ptr+num_print,std::ostream_iterator<double>(std::cout, " "));std::cout<<std::endl; 
-    std::cout<<"computed speed (in m/s):"<<std::endl;
-    thrust::copy(traj_speed_ptr,traj_speed_ptr+num_print,std::ostream_iterator<double>(std::cout, " "));std::cout<<std::endl; 
+    std::vector<double> gpu_distance(distance_speed.first.size);
+    cudaMemcpy(gpu_distance.data(), distance_speed.first.data,
+               distance_speed.first.size * sizeof(double), cudaMemcpyDefault);
+    std::vector<double> gpu_speed(distance_speed.second.size);
+    cudaMemcpy(gpu_speed.data(), distance_speed.second.data,
+               distance_speed.second.size * sizeof(double), cudaMemcpyDefault);
+    EXPECT_THAT(gpu_distance, testing::Pointwise(FloatNearPointwise(1e-8),
+                distance));
+    EXPECT_THAT(gpu_speed, testing::Pointwise(FloatNearPointwise(1e-8),
+                speed));
 }
