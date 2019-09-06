c26888602c7ddc1e9662ee6f7226904db8c9172c
Dillon Cullinan <dcullinan@dgx13.aselab.nvidia.com>
2019-09-06 09:53:41 -0700

    Remove git clean, it removes cudf repo

--- conda/recipes/libcuspatial/build.sh
+++ conda/recipes/libcuspatial/build.sh
@@ -3,8 +3,6 @@
 # This assumes the script is executed from the root of the repo directory
 # show environment
 printenv
-# Cleanup local git
-git clean -xdf
 # build cudf with verbose output
 cd $CUDF_HOME
 ./build.sh -v libnvstrings nvstrings libcudf cudf
