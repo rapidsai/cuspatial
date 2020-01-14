41c5f511323cdfca4da852b1568d78b0c4e162a1
H. Thomson Comer <thomcom@gmail.com>
2020-01-14 13:50:20 -0800

    Get its_timestamp tests working again.

--- python/cuspatial/cuspatial/tests/test_its_timestamp_to_series.py
+++ python/cuspatial/cuspatial/tests/test_its_timestamp_to_series.py
@@ -8,27 +8,27 @@ from cuspatial.utils.traj_utils import its_timestamp_int64_to_datetime64ms
 
 def test_zero():
     assert_eq(
-        its_timestamp_int64_to_datetime64ms(cudf.Series([0]).data.mem),
+        its_timestamp_int64_to_datetime64ms(cudf.Series([0])._column),
         cudf.Series([0]).astype("datetime64[ms]"),
     )
 
 
 def test_one():
     assert_eq(
-        its_timestamp_int64_to_datetime64ms(cudf.Series([1]).data.mem),
+        its_timestamp_int64_to_datetime64ms(cudf.Series([1])._column),
         cudf.Series(["1971-01-01"]).astype("datetime64[ms]"),
     )
 
 
 def test_zeroes():
     assert_eq(
-        its_timestamp_int64_to_datetime64ms(cudf.Series([0, 0]).data.mem),
+        its_timestamp_int64_to_datetime64ms(cudf.Series([0, 0])._column),
         cudf.Series([0, 0]).astype("datetime64[ms]"),
     )
 
 
 def test_ones():
     assert_eq(
-        its_timestamp_int64_to_datetime64ms(cudf.Series([1, 1]).data.mem),
+        its_timestamp_int64_to_datetime64ms(cudf.Series([1, 1])._column),
         cudf.Series(["1971-01-01", "1971-01-01"]).astype("datetime64[ms]"),
     )
