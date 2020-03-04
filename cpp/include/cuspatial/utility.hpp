174ef28df8ade4f0e579fc0dd696e54ae8311991
H. Thomson Comer <thomcom@gmail.com>
2020-03-04 09:44:47 -0600

    Move utility header

---
+++ cpp/include/cuspatial/utility.hpp
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2020, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "cusparse.h"
+
+namespace detail {
+
+void HANDLE_CUSPARSE_STATUS(cusparseStatus_t status) {
+  if(status != CUSPARSE_STATUS_SUCCESS) {
+    const char* status_string;
+    switch(status) {
+      case CUSPARSE_STATUS_SUCCESS:
+          status_string = "CUSPARSE_STATUS_SUCCESS";
+          break;
+      case CUSPARSE_STATUS_NOT_INITIALIZED:
+          status_string = "CUSPARSE_STATUS_NOT_INITIALIZED";
+          break;
+      case CUSPARSE_STATUS_ALLOC_FAILED:
+          status_string = "CUSPARSE_STATUS_ALLOC_FAILED";
+          break;
+      case CUSPARSE_STATUS_INVALID_VALUE:
+          status_string = "CUSPARSE_STATUS_INVALID_VALUE";
+          break;
+      case CUSPARSE_STATUS_ARCH_MISMATCH:
+          status_string = "CUSPARSE_STATUS_ARCH_MISMATCH";
+          break;
+      case CUSPARSE_STATUS_EXECUTION_FAILED:
+          status_string = "CUSPARSE_STATUS_EXECUTION_FAILED";
+          break;
+      case CUSPARSE_STATUS_INTERNAL_ERROR:
+          status_string = "CUSPARSE_STATUS_INTERNAL_ERROR";
+          break;
+      case CUSPARSE_STATUS_MATRIX_TYPE_NOT_SUPPORTED:
+          status_string = "CUSPARSE_STATUS_MATRIX_TYPE_NOT_SUPPORTED";
+          break;
+      default:
+          status_string = "UNKNOWN";
+    }
+    printf("Cusparse error status %s\n", status_string);
+    CUDF_EXPECTS(status != CUSPARSE_STATUS_SUCCESS, "Fail");
+  }
+}
+
+static void HandleCudaError( cudaError_t err,
+                         const char *file,
+                         int line ) {
+    if (err != cudaSuccess) {
+        printf( "%s in %s at line %d\n", cudaGetErrorString( err ),
+                file, line );
+        exit( EXIT_FAILURE );
+    }
+}
+#define HANDLE_CUDA_ERROR( err ) (detail::HandleCudaError( err, __FILE__, __LINE__ ))
+
+} // detail namespace
+
+// Macro block for printing cudf::columns
+
+#define ALLOW_PRINT 0
+#if ALLOW_PRINT
+
+template<typename T>
+void tPrint(thrust::detail::normal_iterator<T> start, thrust::detail::normal_iterator<T> stop, const char* name="None") {
+    std::cout << name << " size: " << stop-start << " ";
+    thrust::copy(start, stop, std::ostream_iterator<float>(std::cout, " "));
+    std::cout << "\n";
+}
+
+template<typename T>
+void tPrint(rmm::device_vector<T> vec, const char* name="None") {
+  tPrint(vec.begin(), vec.end(), name);
+}
+
+void tPrint(cudf::mutable_column_view col, const char* name="None") {
+  rmm::device_vector<float> vec = rmm::device_vector<float>(col.data<float>(), col.data<float>()+col.size());
+  tPrint(vec.begin(), vec.end(), name);
+}
+
+void tPrint(cudf::column_view col, const char* name="None") {
+  rmm::device_vector<float> vec = rmm::device_vector<float>(col.data<float>(), col.data<float>()+col.size());
+  tPrint(vec.begin(), vec.end(), name);
+}
+#define TPRINT(vec, name) (tPrint( vec, name))
+
+#else
+#define TPRINT(vec, name) {}
+#endif
+
