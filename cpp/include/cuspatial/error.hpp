e82d4e0d3808d6d3a74477d3a78367f923517d57
Christopher Harris <charris@nvidia.com>
2020-04-02 18:23:28 -0500

    changelog

--- cpp/include/cuspatial/error.hpp
+++ cpp/include/cuspatial/error.hpp
@@ -30,21 +30,30 @@ inline void throw_cusparse_error(cusparseStatus_t error,
                                  const char* file,
                                  unsigned int line)
 {
-  // would be nice to include `cusparseGetErrorName(error)` and
-  // `cusparseGetErrorString(error)`, but those aren't introduced until
-  // cuda 10.1 (and not in the initial release).
-  throw cuspatial::cusparse_error(
-      std::string{"CUSPARSE error encountered at: " + std::string{file} + ":" +
-                  std::to_string(line) + ": " + std::to_string(error)});
+    // would be nice to include `cusparseGetErrorName(error)` and
+    // `cusparseGetErrorString(error)`, but those aren't introduced until
+    // cuda 10.1 (and not in the initial release).
+    throw cuspatial::cusparse_error(
+        std::string{"CUSPARSE error encountered at: " + std::string{file} + ":" +
+                    std::to_string(line) + ": " + std::to_string(error)});
 }
 
-#define CUSPARSE_TRY(call)                                                 \
-  do {                                                                     \
-    cusparseStatus_t status = (call);                                      \
-    if (CUSPARSE_STATUS_SUCCESS != status) {                               \
-      cuspatial::detail::throw_cusparse_error(status, __FILE__, __LINE__); \
-    }                                                                      \
-  } while (0);
+#define CUSPARSE_TRY(call)                                                   \
+    do {                                                                     \
+        cusparseStatus_t status = (call);                                    \
+        if (CUSPARSE_STATUS_SUCCESS != status) {                             \
+        cuspatial::detail::throw_cusparse_error(status, __FILE__, __LINE__); \
+        }                                                                    \
+    } while (0);
 
 } // namespace detail
+
+#define CUSPATIAL_EXPECTS(cond, reason)                                               \
+    (!!(cond))                                                                        \
+        ? static_cast<void>(0)                                                        \
+        : throw cuspatial::logic_error("cuSpatial failure at: " __FILE__              \
+                                       ":" CUSPATIAL_STRINGIFY(__LINE__) ": " reason)
+
+#define CUSPATIAL_FAIL(reason) CUSPATIAL_EXPECTS(false, reason)
+
 } // namespace cuspatial
