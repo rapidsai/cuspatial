0c4be6c7f611560de4529474c44acb84141697cd
Christopher Harris <charris@nvidia.com>
2020-04-10 00:06:38 -0500

    add ported hausdorff.hpp

---
+++ cpp/include/cuspatial/hausdorff.hpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2019, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <memory>
+#include <cudf/types.h>
+
+namespace cuspatial {
+
+/**
+ * @brief compute Hausdorff distances among all pairs of a set of trajectories
+ * 
+ * https://en.wikipedia.org/wiki/Hausdorff_distance
+ *
+ * `vertex_counts` is used to compute the starting offset of each trajectory
+ * in `x` and `y`
+ * 
+ * @param[in] x: x coordinates of the input trajectories
+ * @param[in] y: y coordinates of the input trajectories
+ * @param[in] vertex_counts: numbers of vertices in each trajectory
+ *
+ * @returns Flattened (1D) column of all-pairs directed Hausdorff distances
+ *          among trajectories (i,j)
+ * 
+ * @note Hausdorff distance is not symmetrical
+ */
+std::unique_ptr<cudf::column>
+directed_hausdorff_distance(cudf::column_view const& x,
+                            cudf::column_view const& y,
+                            cudf::column_view const& vertex_counts,
+                            rmm::mr::device_memory_resource *mr =
+                              rmm::mr::get_default_resource());
+
+}  // namespace cuspatial
---
+++ cpp/src/spatial/hausdorff.cu
@@ -0,0 +1,178 @@
+/*
+ * Copyright (c) 2019, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <memory>
+#include <type_traits>
+#include <cudf/types.hpp>
+#include <cudf/utilities/type_dispatcher.hpp>
+#include <cudf/column/column.hpp>
+#include <cudf/column/column_device_view.cuh>
+#include <cudf/column/column_factories.hpp>
+#include <cudf/column/column_view.hpp>
+#include <cuspatial/error.hpp>
+
+namespace {
+
+// const uint32_t NUM_THREADS = 1024;
+
+template<typename T>
+constexpr auto magnitude_squared(T a, T b) {
+    return a * a + b * b;
+}
+
+template <typename T>
+__global__ void kernel_hausdorff_full(int trajectory_count,
+                                      T* xs,
+                                      T* ys,
+                                      uint32_t* pos,
+                                      T* results)
+{
+    int block_idx = blockIdx.y * gridDim.x + blockIdx.x;
+
+    if (block_idx < trajectory_count * trajectory_count)
+    {
+        int a_idx   = block_idx % trajectory_count;
+        int a_begin = a_idx == 0 ? 0 : pos[a_idx - 1];
+        int a_end   =                  pos[a_idx];
+
+        int b_idx   = block_idx / trajectory_count;
+        int b_begin = b_idx == 0 ? 0 : pos[b_idx - 1];
+        int b_end   =                  pos[b_idx];
+
+        T min_dist_sqrd = 1e20;
+
+        int max_threads = b_end - b_begin; // why b vs a? minimum length of the two, I guess?
+
+        if (threadIdx.x < max_threads)
+        {
+            T bx = xs[b_begin + threadIdx.x];
+            T by = ys[b_begin + threadIdx.x];
+
+            for (int i = a_begin; i < a_end; i++)
+            {
+                T ax = xs[i];
+                T ay = ys[i];
+                T dist_sqrd = magnitude_squared(bx - ax, by - ay);
+                min_dist_sqrd = min(min_dist_sqrd, dist_sqrd);
+            }
+        }
+
+        if (min_dist_sqrd > 1e10) // this may make some of the following statements uncessary.
+        {
+            min_dist_sqrd = -1;
+        }
+
+        __shared__ T dist_sqrd_shared[1024];
+
+        dist_sqrd_shared[threadIdx.x] = -1;
+
+        __syncthreads(); // not sure if this is necessary
+
+        if (threadIdx.x < max_threads) // not sure why this condition exists.
+        {
+            dist_sqrd_shared[threadIdx.x] = min_dist_sqrd;
+        }
+
+        __syncthreads(); // not sure if this is necessary
+
+        for (int offset = blockDim.x / 2; offset > 0; offset >>= 1)
+        {
+            if (threadIdx.x < offset)
+            {
+                dist_sqrd_shared[threadIdx.x] = max(dist_sqrd_shared[threadIdx.x],
+                                                    dist_sqrd_shared[threadIdx.x + offset]);
+            }
+
+            __syncthreads(); // in a loop? yikes!
+        }
+
+        __syncthreads(); // but we just did this, right?
+
+        if (threadIdx.x == 0)
+        {
+            // can maybe avoid this conditional by using a negate and abs trick.
+            //                 = sqrt(abs(max(-1e10, dist_sqrd_shared[0]);
+            results[block_idx] = (dist_sqrd_shared[0] < 0) ? 1e10 : sqrt(dist_sqrd_shared[0]);
+        }
+    }
+}
+
+struct hausdorff_functor
+{
+    template<typename T, typename... Args>
+    std::enable_if_t<not std::is_floating_point<T>::value, std::unique_ptr<cudf::column>>
+    operator()(Args&&...)
+    {
+        CUSPATIAL_FAIL("Non-floating point operation is not supported");
+    }
+
+    template<typename T>
+    std::enable_if_t<std::is_floating_point<T>::value, std::unique_ptr<cudf::column>>
+    operator()(cudf::column_view const& x,
+               cudf::column_view const& y,
+               cudf::column_view const& trajectory_lengths,
+               rmm::mr::device_memory_resource *mr,
+               cudaStream_t stream)
+    {
+        auto tid = cudf::experimental::type_to_id<T>();
+        auto result = cudf::make_fixed_width_column(cudf::data_type{ tid },
+                                                    trajectory_lengths.size() * trajectory_lengths.size(),
+                                                    cudf::mask_state::UNALLOCATED,
+                                                    stream,
+                                                    mr);
+
+        auto d_x = cudf::column_device_view::create(x);
+        auto d_y = cudf::column_device_view::create(y);
+        auto d_trajectory_lengths = cudf::column_device_view::create(trajectory_lengths);
+        auto d_trajectory_offsets = rmm::device_vector<cudf::size_type>(trajectory_lengths.size());
+
+        thrust::exclusive_scan(rmm::exec_policy(stream)->on(stream),
+                                                d_trajectory_lengths->begin<cudf::size_type>(),
+                                                d_trajectory_lengths->end<cudf::size_type>(),
+                                                d_trajectory_offsets.begin());
+
+        return result;
+    }
+};
+
+} // namespace anonymous
+
+namespace cuspatial {
+
+std::unique_ptr<cudf::column>
+directed_hausdorff_distance(cudf::column_view const& x,
+                            cudf::column_view const& y,
+                            cudf::column_view const& trajector_lengths,
+                            rmm::mr::device_memory_resource *mr)
+{
+    CUSPATIAL_EXPECTS(x.type() == y.type(), "inputs `x` and `y` must have same type.");
+    CUSPATIAL_EXPECTS(x.size() == y.size(), "inputs `x` and `y` must have same length.");
+
+    CUSPATIAL_EXPECTS(not x.has_nulls() and
+                      not y.has_nulls() and
+                      not trajector_lengths.has_nulls(),
+                      "inputs must not have nulls.");
+
+    CUSPATIAL_EXPECTS(x.size() >= trajector_lengths.size(),
+                      "At least one vertex is required for each trajectory");
+
+    cudaStream_t stream = 0;
+
+    return cudf::experimental::type_dispatcher(x.type(), hausdorff_functor(),
+                                               x, y, trajector_lengths, mr, stream);
+}
+
+} // namespace cuspatial
