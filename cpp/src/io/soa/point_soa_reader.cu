be378b74302b937ef5e0fee285b79f1b3e673f9d
H. Thomson Comer <thomcom@gmail.com>
2020-04-17 14:45:24 -0500

    Refactor point_soa_reader.cu

--- cpp/src/io/soa/point_soa_reader.cu
+++ cpp/src/io/soa/point_soa_reader.cu
@@ -19,9 +19,10 @@
 #include <math.h>
 #include <cuda_runtime.h>
 #include <cudf/types.h>
-#include <cudf/legacy/column.hpp>
+#include <cudf/column/column.hpp>
 #include <cudf/utilities/error.hpp>
-#include <rmm/rmm.h>
+#include <cudf/utilities/type_dispatcher.hpp>
+#include <rmm/thrust_rmm_allocator.h>
 #include <cuspatial/soa_readers.hpp>
 #include <utility/utility.hpp>
 
@@ -29,50 +30,61 @@ namespace cuspatial
 {
 namespace detail
 {
-    std::pair<cudf::column, cudf::column> read_lonlat_points_soa(const char *filename,
-        cudaStream_t stream, rmm::device_memory_resource* mr)
-    {
-        // Read the lon and lat points from the soa file into host memory
-        double* p_lon=nullptr, *p_lat=nullptr;
-        int num_p = read_point_lonlat<double>(filename, p_lon, p_lat);
 
-        // Allocate a cudf::column with the lon host memory
-        auto lon = cudf::test::fixed_width_column_wrapper<double>(p_lon, stream, mr);
-        auto lat = cudf::test::fixed_width_column_wrapper<double>(p_lat, stream, mr);
+std::pair<std::unique_ptr<cudf::column>, std::unique_ptr<cudf::column>> read_lonlat_points_soa(const char *filename,
+    cudaStream_t stream, rmm::mr::device_memory_resource* mr)
+{
+    // Read the lon and lat points from the soa file into host memory
+    double* p_lon=nullptr, *p_lat=nullptr;
+    int num_p = read_point_lonlat<double>(filename, p_lon, p_lat);
 
-        return std::pair<lon, lat>
-    }
+    auto tid = cudf::experimental::type_to_id<double>();
+    auto type = cudf::data_type{ tid };
+
+    // Allocate a cudf::column with the lon host memory
+    auto lon = std::make_unique<cudf::column>(cudf::column(type, num_p, rmm::device_buffer(p_lon, num_p)));
+    auto lat = std::make_unique<cudf::column>(cudf::column(type, num_p, rmm::device_buffer(p_lat, num_p)));
+
+    return std::make_pair(std::move(lon), std::move(lat));
+}
+
+std::pair<std::unique_ptr<cudf::column>, std::unique_ptr<cudf::column>> read_xy_points_soa(const char* filename,
+    cudaStream_t stream, rmm::mr::device_memory_resource* mr)
+{
+    double * p_x=nullptr, *p_y=nullptr;
+    int num_p = read_point_xy<double>(filename,p_x,p_y);
+
+    auto tid = cudf::experimental::type_to_id<double>();
+    auto type = cudf::data_type{ tid };
+
+    auto x = std::make_unique<cudf::column>(cudf::column(type, num_p, rmm::device_buffer(p_x, num_p)));
+    auto y = std::make_unique<cudf::column>(cudf::column(type, num_p, rmm::device_buffer(p_y, num_p)));
    
-    std::pair<cudf::column, cudf::column> read_xy_points_soa(const char* filename,
-        cudaStream_t stream, rmm::device_memory_resource* mr) 
-    {
-        double * p_x=nullptr, *p_y=nullptr;
-        int num_p = read_point_xy<double>(filename,p_x,p_y);
+    return std::make_pair(std::move(x), std::move(y));
+}
 
-        auto x = cudf::test::fixed_width_column_wrapper<double>(p_x, stream, mr);
-        auto y = cudf::test::fixed_width_column_wrapper<double>(p_y, stream, mr);
-       
-        return std::make_pair(x, y);
-    }	
 } // detail namespace
 
-    /**
-    * @brief read lon/lat from file into two columns; data type is fixed to double (GDF_FLOAT64)
-    *
-    * see soa_readers.hpp
-    */
-    std::pair<cudf::column, cudf::column> read_lonlat_points_soa(const char *filename)
-    {
-      return detail::read_lonlat_points_soa(filename, cudaStream_t{0}, rmm::mr::get_default_resource());
-    }
-	
-    /**
-    * @brief read x/y from file into two columns; data type is fixed to double (GDF_FLOAT64)
-    *
-    * see soa_readers.hpp
-    */
-    std::pair<cudf::column, cudf::column> read_xy_points_soa(const char *filename)                              
-    {
-      return detail::read_xy_points_soa(filename, cudaStream_t{0}, rmm::mr::get_default_resource());
-    }	
+/**
+* @brief read lon/lat from file into two columns; data type is fixed to double (GDF_FLOAT64)
+*
+* see soa_readers.hpp
+*/
+std::pair<std::unique_ptr<cudf::column>, std::unique_ptr<cudf::column>> read_lonlat_points_soa(const char *filename)
+{
+    auto result = detail::read_lonlat_points_soa(filename, cudaStream_t{0}, rmm::mr::get_default_resource());
+    return result;
 }
+
+/**
+* @brief read x/y from file into two columns; data type is fixed to double (GDF_FLOAT64)
+*
+* see soa_readers.hpp
+*/
+std::pair<std::unique_ptr<cudf::column>, std::unique_ptr<cudf::column>> read_xy_points_soa(const char *filename)
+{
+    auto result = detail::read_xy_points_soa(filename, cudaStream_t{0}, rmm::mr::get_default_resource());
+    return result;
+}
+
+} // cuspatial namespace
