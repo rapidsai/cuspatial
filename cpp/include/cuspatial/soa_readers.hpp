43cd00f1b6d1417b5f06cc76b3a39163bcabc9ad
H. Thomson Comer <thomcom@gmail.com>
2020-04-28 16:39:22 -0500

    Apply suggestions from code review
    Co-Authored-By: Mark Harris <mharris@nvidia.com>

--- cpp/include/cuspatial/soa_readers.hpp
+++ cpp/include/cuspatial/soa_readers.hpp
@@ -70,20 +70,19 @@ std::pair<std::unique_ptr<cudf::column>, std::unique_ptr<cudf::column>>
 read_xy_points_soa(const char *filename, rmm::mr::device_memory_resource* mr);
 
 /**
- * @brief read polygon data from file in SoA format
+ * @brief Read polygon data from file.
  * 
  *
- * @param[in] filename: polygon data filename
+ * @param[in] filepath path to file.
+ * @param[in] mr Optional resource to use for allocating output device memory.
  *
  * @note x/y can also be longitude and latitude.
  *
- * @return: vector of columns
- *          column(0): index polygons: prefix sum of number of rings of all
- *                     polygons
+ * @return `std::vector` of `cudf::column`s:
+ *          column(0): index polygons: INT64 offsets to the start of each polygon. The size of this column equals the number of polygons.
  *          column(1): index rings: INT64 offset to the start of each ring in the vertex data.
- *                     rings
- *          column(2): x coordinates of concatenated polygons
- *          column(3): y coordinates of concatenated polygons
+ *          column(2): FLOAT64 x-coordinates of concatenated polygons.
+ *          column(3): FLOAT64 y-coordinates of concatenated polygons.
 **/
 std::vector<cudf::column>
 read_polygons(std::string filepath, 
