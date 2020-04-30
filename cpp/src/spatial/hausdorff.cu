3b4c2013a61e16f8bd68c7780db2048d1bb2c45c
Christopher Harris <charris@nvidia.com>
2020-04-30 01:10:21 -0500

    hausdorff: clarify variable names

--- cpp/src/spatial/hausdorff.cu
+++ cpp/src/spatial/hausdorff.cu
@@ -26,8 +26,6 @@
 
 namespace {
 
-const uint32_t NUM_THREADS = 1024;
-
 template<typename T>
 constexpr auto magnitude_squared(T a, T b) {
     return a * a + b * b;
@@ -35,79 +33,70 @@ constexpr auto magnitude_squared(T a, T b) {
 
 template <typename T>
 __global__ void kernel_hausdorff(int num_spaces,
-                                      T const* xs,
-                                      T const* ys,
-                                      cudf::size_type* space_offsets,
-                                      T* results)
+                                 T const* xs,
+                                 T const* ys,
+                                 cudf::size_type* space_offsets,
+                                 T* results)
 {
     auto block_idx = blockIdx.y * gridDim.x + blockIdx.x;
-    auto num_space_pairs = num_spaces * num_spaces;
+    auto num_results = num_spaces * num_spaces;
 
-    // each block processes a single pair of spaces.
-    if (block_idx < num_space_pairs)
+    // each block processes a single result / pair of spaces
+    if (block_idx >= num_results)
     {
-        int space_a_idx   = block_idx % num_spaces;
-        int space_a_begin = space_a_idx == 0 ? 0 : space_offsets[space_a_idx - 1];
-        int space_a_end   =                        space_offsets[space_a_idx];
+        return;
+    }
 
-        int space_b_idx   = block_idx / num_spaces;
-        int space_b_begin = space_b_idx == 0 ? 0 : space_offsets[space_b_idx - 1];
-        int space_b_end   =                        space_offsets[space_b_idx];
+    int space_a_idx   = block_idx % num_spaces;
+    int space_a_begin = space_a_idx == 0 ? 0 : space_offsets[space_a_idx - 1];
+    int space_a_end   =                        space_offsets[space_a_idx];
 
-        T min_dist_sqrd = 1e20;
+    int space_b_idx   = block_idx / num_spaces;
+    int space_b_begin = space_b_idx == 0 ? 0 : space_offsets[space_b_idx - 1];
+    int space_b_end   =                        space_offsets[space_b_idx];
 
-        int num_points_in_b = space_b_end - space_b_begin;
+    T min_dist_sqrd = 1e20;
 
-        if (threadIdx.x < num_points_in_b)
+    int num_points_in_b = space_b_end - space_b_begin;
+
+    if (threadIdx.x < num_points_in_b)
+    {
+        T point_b_x = xs[space_b_begin + threadIdx.x];
+        T point_b_y = ys[space_b_begin + threadIdx.x];
+
+        for (int i = space_a_begin; i < space_a_end; i++)
         {
-            T point_b_x = xs[space_b_begin + threadIdx.x];
-            T point_b_y = ys[space_b_begin + threadIdx.x];
+            T point_a_x = xs[i];
+            T point_a_y = ys[i];
+            T dist_sqrd = magnitude_squared(point_b_x - point_a_x, point_b_y - point_a_y);
 
-            for (int i = space_a_begin; i < space_a_end; i++)
-            {
-                T point_a_x = xs[i];
-                T point_a_y = ys[i];
-                T dist_sqrd = magnitude_squared(point_b_x - point_a_x, point_b_y - point_a_y);
-
-                min_dist_sqrd = min(min_dist_sqrd, dist_sqrd);
-            }
+            min_dist_sqrd = min(min_dist_sqrd, dist_sqrd);
         }
+    }
 
-        if (min_dist_sqrd > 1e10)
-        {
-            min_dist_sqrd = -1;
-        }
+    if (min_dist_sqrd > 1e10)
+    {
+        min_dist_sqrd = -1;
+    }
 
-        __shared__ T dist_sqrd[1024];
+    __shared__ T dist_sqrd[1024];
 
-        dist_sqrd[threadIdx.x] = -1;
+    dist_sqrd[threadIdx.x] = threadIdx.x < num_points_in_b ? min_dist_sqrd : -1;
 
+    for (int offset = blockDim.x / 2; offset > 0; offset >>= 1)
+    {
         __syncthreads();
 
-        if (threadIdx.x < num_points_in_b)
+        if (threadIdx.x < offset)
         {
-            dist_sqrd[threadIdx.x] = min_dist_sqrd;
+            dist_sqrd[threadIdx.x] = max(dist_sqrd[threadIdx.x],
+                                            dist_sqrd[threadIdx.x + offset]);
         }
+    }
 
-        __syncthreads();
-
-        for (int offset = blockDim.x / 2; offset > 0; offset >>= 1)
-        {
-            if (threadIdx.x < offset)
-            {
-                dist_sqrd[threadIdx.x] = max(dist_sqrd[threadIdx.x],
-                                             dist_sqrd[threadIdx.x + offset]);
-            }
-
-            __syncthreads();
-        }
-
-        __syncthreads();
-
-        if (threadIdx.x == 0)
-        {
-            results[block_idx] = (dist_sqrd[0] < 0) ? 1e10 : sqrt(dist_sqrd[0]);
-        }
+    if (threadIdx.x == 0)
+    {
+        results[block_idx] = (dist_sqrd[0] < 0) ? 1e10 : sqrt(dist_sqrd[0]);
     }
 }
 
@@ -122,15 +111,15 @@ struct hausdorff_functor
 
     template<typename T>
     std::enable_if_t<std::is_floating_point<T>::value, std::unique_ptr<cudf::column>>
-    operator()(cudf::column_view const& x,
-               cudf::column_view const& y,
-               cudf::column_view const& trajectory_lengths,
+    operator()(cudf::column_view const& xs,
+               cudf::column_view const& ys,
+               cudf::column_view const& points_per_space,
                rmm::mr::device_memory_resource *mr,
                cudaStream_t stream)
     {
         auto tid = cudf::experimental::type_to_id<T>();
         auto result = cudf::make_fixed_width_column(cudf::data_type{ tid },
-                                                    trajectory_lengths.size() * trajectory_lengths.size(),
+                                                    points_per_space.size() * points_per_space.size(),
                                                     cudf::mask_state::UNALLOCATED,
                                                     stream,
                                                     mr);
@@ -140,35 +129,29 @@ struct hausdorff_functor
             return result;
         }
 
-        auto d_x = cudf::column_device_view::create(x);
-        auto d_y = cudf::column_device_view::create(y);
-        auto d_trajectory_lengths = cudf::column_device_view::create(trajectory_lengths);
-        auto d_space_offsets = rmm::device_vector<cudf::size_type>(trajectory_lengths.size());
+        auto space_offsets = rmm::device_vector<cudf::size_type>(points_per_space.size());
 
         thrust::inclusive_scan(rmm::exec_policy(stream)->on(stream),
-                                                d_trajectory_lengths->begin<cudf::size_type>(),
-                                                d_trajectory_lengths->end<cudf::size_type>(),
-                                                d_space_offsets.begin());
+                               points_per_space.begin<cudf::size_type>(),
+                               points_per_space.end<cudf::size_type>(),
+                               space_offsets.begin());
 
-        auto kernel = kernel_hausdorff<T>;
+        // utilize one block per result (pair of spaces).
+        int max_block_size = 65535;
+        int block_x = min(result->size(), max_block_size);
+        int block_y = ceil(result->size() / (float) max_block_size);
 
-        int block_x = result->size();
-        int block_y = 1;
-
-        if (result->size() > 65535)
-        {
-            block_y = ceil((float) result->size() / 65535.0);
-            block_x = 65535;
-        }
-
+        const uint32_t NUM_THREADS = 1024;
         dim3 grid(block_x, block_y);
         dim3 block(NUM_THREADS);
 
+        auto kernel = kernel_hausdorff<T>;
+
         kernel<<<grid, block, 0, stream>>>(
-            trajectory_lengths.size(),
-            x.data<T>(),
-            y.data<T>(),
-            d_space_offsets.data().get(),
+            points_per_space.size(),
+            xs.data<T>(),
+            ys.data<T>(),
+            space_offsets.data().get(),
             result->mutable_view().data<T>()
         );
 
@@ -181,26 +164,26 @@ struct hausdorff_functor
 namespace cuspatial {
 
 std::unique_ptr<cudf::column>
-directed_hausdorff_distance(cudf::column_view const& x,
-                            cudf::column_view const& y,
-                            cudf::column_view const& trajectory_lengths,
+directed_hausdorff_distance(cudf::column_view const& xs,
+                            cudf::column_view const& ys,
+                            cudf::column_view const& points_per_space,
                             rmm::mr::device_memory_resource *mr)
 {
-    CUSPATIAL_EXPECTS(x.type() == y.type(), "inputs `x` and `y` must have same type.");
-    CUSPATIAL_EXPECTS(x.size() == y.size(), "inputs `x` and `y` must have same length.");
+    CUSPATIAL_EXPECTS(xs.type() == ys.type(), "inputs `xs` and `ys` must have same type.");
+    CUSPATIAL_EXPECTS(xs.size() == ys.size(), "inputs `xs` and `ys` must have same length.");
 
-    CUSPATIAL_EXPECTS(not x.has_nulls() and
-                      not y.has_nulls() and
-                      not trajectory_lengths.has_nulls(),
+    CUSPATIAL_EXPECTS(not xs.has_nulls() and
+                      not ys.has_nulls() and
+                      not points_per_space.has_nulls(),
                       "inputs must not have nulls.");
 
-    CUSPATIAL_EXPECTS(x.size() >= trajectory_lengths.size(),
+    CUSPATIAL_EXPECTS(xs.size() >= points_per_space.size(),
                       "At least one vertex is required for each trajectory");
 
     cudaStream_t stream = 0;
 
-    return cudf::experimental::type_dispatcher(x.type(), hausdorff_functor(),
-                                               x, y, trajectory_lengths, mr, stream);
+    return cudf::experimental::type_dispatcher(xs.type(), hausdorff_functor(),
+                                               xs, ys, points_per_space, mr, stream);
 }
 
 } // namespace cuspatial
