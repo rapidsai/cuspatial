5a51dd3a78e3e033b8e597442b9559377f94968c
Christopher Harris <charris@nvidia.com>
2020-05-08 01:56:47 -0500

    hausdorff: improve documentation

--- cpp/include/cuspatial/hausdorff.hpp
+++ cpp/include/cuspatial/hausdorff.hpp
@@ -22,18 +22,56 @@
 namespace cuspatial {
 
 /**
- * @brief computes Hausdorff distances for all pairs of a collection of spaces
- * 
+ * @brief computes Hausdorff distances for all pairs in a collection of spaces
+ *
  * https://en.wikipedia.org/wiki/Hausdorff_distance
  *
- * `points_per_space` is used to compute the offset of the first point in each space.
- * 
- * @param[in] xs: x coordinate of points in space
- * @param[in] ys: y coordinate of points in space
+ * Example in 1D (this function operates in 2D):
+ *
+ * spaces
+ * [0 2 5] [9] [3 7]
+ *
+ * spaces represented as points per space and concatenation of all points
+ * [0 2 5 9 3 7] [3 1 2]
+ *
+ * cartesian product of all
+ * points visually seperated
+ * by pair of spaces            distance between points
+ * +----------+----+-------+    +---------+---+------+
+ * : 00 02 05 : 09 : 03 07 :    : 0  2  5 : 9 : 3  7 :
+ * : 20 22 25 : 29 : 23 27 :    : 2  0  3 : 7 : 1  5 :
+ * : 50 52 55 : 59 : 53 57 :    : 5  3  0 : 4 : 2  2 :
+ * +----------+----+-------+    +---------+---+------+
+ * : 90 92 95 : 99 : 93 97 :    : 9  7  4 : 0 : 6  2 :
+ * +----------+----+-------+    +---------+---+------+
+ * : 30 32 35 : 39 : 33 37 :    : 3  1  2 : 6 : 0  4 :
+ * : 70 72 75 : 79 : 73 77 :    : 7  5  2 : 2 : 4  0 :
+ * +----------+----+-------+    +---------+---+------+
+
+ * minimum distance from
+ * every point in one           Hausdorff distance is
+ * space to any point in        the maximum of the
+ * the other space              minimum distances
+ * +----------+----+-------+    +---------+---+------+
+ * :  0       :  9 :  3    :    : 0       : 9 : 3    :
+ * :     0    :  7 :  1    :    :         :   :      :
+ * :        0 :  4 :  2    :    :         :   :      :
+ * +----------+----+-------+    +---------+---+------+
+ * :        4 :  0 :     2 :    :       4 : 0 :    2 :
+ * +----------+----+-------+    +---------+---+------+
+ * :     1    :  6 :  0    :    :         : 6 : 0    :
+ * :        2 :  2 :     0 :    :       2 :   :      :
+ * +----------+----+-------+    +---------+---+------+
+ *
+ * returned as concatenation of columns
+ * [0 2 4 3 0 2 9 6 0]
+ *
+ * @param[in] xs: x component of points
+ * @param[in] ys: y component of points
  * @param[in] points_per_space: number of points in each space
  *
- * @returns A flattened matrix of all Hausdorff distances for each pair of spaces
- * 
+ * @returns Hausdorff distances for each pair of spaces
+ *
  * @note Hausdorff distances are asymmetrical
  */
 std::unique_ptr<cudf::column>
