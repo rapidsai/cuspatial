ccbae2a7003c9217cec64b6b69fab3d42eb8b146
ptaylor <paul.e.taylor@me.com>
2020-05-13 02:32:39 -0700

    change read_polygon_shapefile return type to vector for Cython compatibility

--- cpp/tests/io/shp/polygon_shapefile_reader_test.cpp
+++ cpp/tests/io/shp/polygon_shapefile_reader_test.cpp
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-
 #include <cuspatial/error.hpp>
 #include <cuspatial/shapefile_reader.hpp>
 #include <tests/utilities/base_fixture.hpp>
@@ -28,11 +27,10 @@ using namespace cudf::test;
 
 std::string get_shapefile_path(std::string filename)
 {
-    const char* cuspatial_home = std::getenv("CUSPATIAL_HOME");
-    CUSPATIAL_EXPECTS(cuspatial_home != nullptr, "CUSPATIAL_HOME environmental variable must be set");
-    return std::string(cuspatial_home)
-         + std::string("/test_fixtures/shapefiles/")
-         + std::string(filename);
+  const char* cuspatial_home = std::getenv("CUSPATIAL_HOME");
+  CUSPATIAL_EXPECTS(cuspatial_home != nullptr, "CUSPATIAL_HOME environmental variable must be set");
+  return std::string(cuspatial_home) + std::string("/test_fixtures/shapefiles/") +
+         std::string(filename);
 }
 
 template <typename T>
@@ -44,51 +42,41 @@ void test(std::string const& shapefile_name,
           std::vector<double> xs,
           std::vector<double> ys)
 {
-    auto shape_filename = get_shapefile_path(shapefile_name);
-    auto polygon_columns = cuspatial::read_polygon_shapefile(shape_filename);
+  auto shape_filename  = get_shapefile_path(shapefile_name);
+  auto polygon_columns = cuspatial::read_polygon_shapefile(shape_filename);
 
-    auto expected_poly_offsets = wrapper<cudf::size_type>(poly_offsets.begin(), poly_offsets.end());
-    auto expected_ring_offsets = wrapper<cudf::size_type>(ring_offsets.begin(), ring_offsets.end());
-    auto expected_poly_point_xs = wrapper<double>(xs.begin(), xs.end());
-    auto expected_poly_point_ys = wrapper<double>(ys.begin(), ys.end());
+  auto expected_poly_offsets  = wrapper<cudf::size_type>(poly_offsets.begin(), poly_offsets.end());
+  auto expected_ring_offsets  = wrapper<cudf::size_type>(ring_offsets.begin(), ring_offsets.end());
+  auto expected_poly_point_xs = wrapper<double>(xs.begin(), xs.end());
+  auto expected_poly_point_ys = wrapper<double>(ys.begin(), ys.end());
 
-    expect_columns_equivalent(expected_poly_offsets, std::get<0>(polygon_columns)->view(), true);
-    expect_columns_equivalent(expected_ring_offsets, std::get<1>(polygon_columns)->view(), true);
-    expect_columns_equivalent(expected_poly_point_xs, std::get<2>(polygon_columns)->view(), true);
-    expect_columns_equivalent(expected_poly_point_ys, std::get<3>(polygon_columns)->view(), true);
+  expect_columns_equivalent(expected_poly_offsets, polygon_columns.at(0)->view(), true);
+  expect_columns_equivalent(expected_ring_offsets, polygon_columns.at(1)->view(), true);
+  expect_columns_equivalent(expected_poly_point_xs, polygon_columns.at(2)->view(), true);
+  expect_columns_equivalent(expected_poly_point_ys, polygon_columns.at(3)->view(), true);
 }
 
-struct PolygonShapefileReaderTest : public BaseFixture {};
+struct PolygonShapefileReaderTest : public BaseFixture {
+};
 
 TEST_F(PolygonShapefileReaderTest, NonExistantFile)
 {
-    auto shape_filename = get_shapefile_path("non_exist.shp");
-    EXPECT_THROW(cuspatial::read_polygon_shapefile(shape_filename), cuspatial::logic_error);
+  auto shape_filename = get_shapefile_path("non_exist.shp");
+  EXPECT_THROW(cuspatial::read_polygon_shapefile(shape_filename), cuspatial::logic_error);
 }
 
-TEST_F(PolygonShapefileReaderTest, ZeroPolygons)
-{
-    test("empty_poly.shp",
-         {},
-         {},
-         {},
-         {});
-}
+TEST_F(PolygonShapefileReaderTest, ZeroPolygons) { test("empty_poly.shp", {}, {}, {}, {}); }
 
 TEST_F(PolygonShapefileReaderTest, OnePolygon)
 {
-    test("one_poly.shp",
-         { 0 },
-         { 0 },
-         { -10,   5, 5, -10, -10 },
-         { -10, -10, 5,   5, -10 });
+  test("one_poly.shp", {0}, {0}, {-10, 5, 5, -10, -10}, {-10, -10, 5, 5, -10});
 }
 
 TEST_F(PolygonShapefileReaderTest, TwoPolygons)
 {
-    test("two_polys.shp",
-         { 0, 1 },
-         { 0, 5 },
-         { -10,   5, 5, -10, -10, 0, 10, 10,  0, 0 },
-         { -10, -10, 5,   5, -10, 0,  0, 10, 10, 0 });
+  test("two_polys.shp",
+       {0, 1},
+       {0, 5},
+       {-10, 5, 5, -10, -10, 0, 10, 10, 0, 0},
+       {-10, -10, 5, 5, -10, 0, 0, 10, 10, 0});
 }
