74a6bca9a10eb9fabd66c2a65802f1363197ce99
Christopher Harris <charris@nvidia.com>
2020-07-06 17:20:40 -0500

    cartesian_product_iterator: improve docs

--- cpp/include/cuspatial/detail/cartesian_product_iterator.cuh
+++ cpp/include/cuspatial/detail/cartesian_product_iterator.cuh
@@ -39,8 +39,8 @@ struct cartesian_product_group_index {
   int32_t element_b_idx;
 };
 
-template <typename OffsetIteratorLHS,
-          typename OffsetIteratorRHS,
+template <typename OffsetIteratorA,
+          typename OffsetIteratorB,
           typename SizeIteratorLHS,
           typename SizeIteratorRHS,
           typename LookupIteratorLHS,
@@ -49,8 +49,8 @@ struct cartesian_product_functor {
   int32_t const num_elements_b;
   SizeIteratorLHS const group_sizes_a;
   SizeIteratorRHS const group_sizes_b;
-  OffsetIteratorLHS const group_offsets_a;
-  OffsetIteratorRHS const group_offsets_b;
+  OffsetIteratorA const group_offsets_a;
+  OffsetIteratorB const group_offsets_b;
   LookupIteratorLHS const group_lookup_a;
   LookupIteratorRHS const group_lookup_b;
 
@@ -94,35 +94,95 @@ struct group_lookup_functor {
   }
 };
 
-template <typename OffsetIteratorLHS, typename OffsetIteratorRHS>
+/**
+ * @brief Makes an iterator of the cartesian product of two iterators. Produces pairs consecutively.
+ *
+ * Pairs of elements grouped by offsets `A` and `B` are produces consecutively.
+ * 1) Each group `A_i` in `A` appears consecutively.
+ * 2) Under each group `A_i`, each group `B_i` in `B` appear consecutively.
+ * 3) Under each group `B_i`, each element within `a` appears consecutively.
+ * 4) Under each element within `A_i`, each element within `B_i` appears consecutively.
+ *
+ * Example:
+ * ```
+ * A_elements = [u, v, w, x, y, z]
+ * B_elements = [0, 1, 2, 3, 4, 5]
+ * A_offsets  = [0, 3, 4]
+ * B_offsets  = [0, 2, 5]
+ * ```
+ * Cartesian Product:
+ *
+ *       B0        B1             B2
+ *     +---------+--------------+----+
+ *  A0 : u0   u1 : u2   u3   u4 : u5 :
+ *     +         :              :    +
+ *     : v0   v1 : v2   v3   v4 : v5 :
+ *     +         :              :    +
+ *     : w0   w1 : w2   w3   w4 : w5 :
+ *     +---------+--------------+----+
+ *  A1 : x0   x1 : x2   x3   x4 : x5 :
+ *     +---------+--------------+----+
+ *  A2 : y0   y1 : y2   y3   y4 : y5 :
+ *     +         :              :    +
+ *     : z0   z1 : z2   z3   z4 : z5 :
+ *     +---------+--------------+----+
+ *
+ * Order of iteration:
+ *
+ *  u0, v0, w0, u1, v1, w1, u2, v2, w2, u3, v3, w3, u4, v4, w4, u5, v5, w5, x0, x1, x2, x3, x4, x5,
+ *  y0, z0, y1, z1, y2, z2, y3, z3, y4, z4, y5, z5
+ *
+ * i.e:
+ *
+ *  A0 * B0 | u0 v0 w0 u1 v1 w1
+ *  A0 * B1 | u2 v2 w2 u3 v3 w3 u4 v4 w4
+ *  A0 * B2 | u5 v5 w5
+ *  A1 * B0 | x0 x1
+ *  A1 * B1 | x2 x3 x4
+ *  A1 * B2 | x5
+ *  A1 * B0 | y0 z0 y1 z1
+ *  A2 * B1 | y2 z2 y3 z3 y4 z4
+ *  A2 * B2 | y5 z5
+ *
+ * @tparam OffsetIteratorA
+ * @tparam OffsetIteratorB
+ * @param num_elements_a  number of elements from all groups in A
+ * @param num_elements_b  number of elements from all groups in B
+ * @param num_groups_a    number of groups in A
+ * @param num_groups_b    number of groups in B
+ * @param group_offsets_a offsets for each group in A, plus an "end" offset
+ * @param group_offsets_b offsets for each group in B, plus an "end" offset
+ * @return auto           Thrust iterator of `cartesian_product_group_index`s. device-only.
+ */
+template <typename OffsetIteratorA, typename OffsetIteratorB>
 auto make_grouped_cartesian_product_iterator(int32_t const num_elements_a,
                                              int32_t const num_elements_b,
                                              int32_t const num_groups_a,
                                              int32_t const num_groups_b,
-                                             OffsetIteratorLHS const group_offsets_a,
-                                             OffsetIteratorRHS const group_offsets_b)
+                                             OffsetIteratorA const group_offsets_a,
+                                             OffsetIteratorB const group_offsets_b)
 {
   auto count_iter = thrust::make_counting_iterator(0);
 
-  using SizeFunctorLHS = detail::size_from_offsets_functor<OffsetIteratorLHS>;
-  using SizeFunctorRHS = detail::size_from_offsets_functor<OffsetIteratorLHS>;
+  using SizeFunctorLHS = detail::size_from_offsets_functor<OffsetIteratorA>;
+  using SizeFunctorRHS = detail::size_from_offsets_functor<OffsetIteratorB>;
 
   auto group_sizes_a = thrust::make_transform_iterator(
     count_iter, SizeFunctorLHS{num_groups_a, num_elements_a, group_offsets_a});
   auto group_sizes_b = thrust::make_transform_iterator(
     count_iter, SizeFunctorRHS{num_groups_b, num_elements_b, group_offsets_b});
   auto group_lookup_a = thrust::make_transform_iterator(
-    count_iter, group_lookup_functor<OffsetIteratorLHS>{group_offsets_a, num_groups_a});
+    count_iter, group_lookup_functor<OffsetIteratorA>{group_offsets_a, num_groups_a});
   auto group_lookup_b = thrust::make_transform_iterator(
-    count_iter, group_lookup_functor<OffsetIteratorLHS>{group_offsets_b, num_groups_b});
+    count_iter, group_lookup_functor<OffsetIteratorB>{group_offsets_b, num_groups_b});
 
   using SizeIteratorLHS   = decltype(group_sizes_a);
   using SizeIteratorRHS   = decltype(group_sizes_b);
   using LookupIteratorLHS = decltype(group_lookup_a);
   using LookupIteratorRHS = decltype(group_lookup_b);
 
-  using TraversalFunctor = cartesian_product_functor<OffsetIteratorLHS,
-                                                     OffsetIteratorRHS,
+  using TraversalFunctor = cartesian_product_functor<OffsetIteratorA,
+                                                     OffsetIteratorB,
                                                      SizeIteratorLHS,
                                                      SizeIteratorRHS,
                                                      LookupIteratorLHS,
@@ -139,6 +199,15 @@ auto make_grouped_cartesian_product_iterator(int32_t const num_elements_a,
   return thrust::make_transform_iterator(count_iter, traversal);
 }
 
+/**
+ * @brief Calls `make_grouped_cartesian_product_iterator` with same inputs for both A and B.
+ *
+ * @tparam OffsetIterator
+ * @param num_elements  number of elements from all groups
+ * @param num_groups    number of groups
+ * @param group_offsets offsets for each group, plus an "end" offset
+ * @return auto         Thrust iterator of `cartesian_product_group_index`s. device-only.
+ */
 template <typename OffsetIterator>
 auto make_grouped_cartesian_product_iterator(int32_t const element_count,
                                              int32_t const group_count,
