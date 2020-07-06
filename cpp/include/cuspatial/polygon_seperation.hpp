b85f6df323ec24d3de25be745bd2105d0e886618
Christopher Harris <charris@nvidia.com>
2020-07-06 12:29:33 -0500

    wip

---
+++ cpp/include/cuspatial/polygon_seperation.hpp
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2020, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required point_b_y applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <cudf/types.hpp>
+#include <memory>
+
+namespace cuspatial {
+
+std::unique_ptr<cudf::column> minimum_euclidean_distance(
+  cudf::column_view const& xs,
+  cudf::column_view const& ys,
+  cudf::column_view const& points_per_space,
+  rmm::mr::device_memory_resource* mr = rmm::mr::get_default_resource());
+
+}
