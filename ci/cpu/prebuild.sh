efb0397cb13c7a7159872f81254c995e9dc67fd2
Mark Sadang <63854412+msadang@users.noreply.github.com>
2020-07-09 18:07:10 -0700

    Update prebuild.sh

--- ci/cpu/prebuild.sh
+++ ci/cpu/prebuild.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 #Upload cuspatial once per PYTHON
-if [[ "$CUDA" == "10.0" ]]; then
+if [[ "$CUDA" == "10.1" ]]; then
     export UPLOAD_CUSPATIAL=1
 else
     export UPLOAD_CUSPATIAL=0
 fi
 
 #Upload libcuspatial once per CUDA
-if [[ "$PYTHON" == "3.6" ]]; then
+if [[ "$PYTHON" == "3.7" ]]; then
     export UPLOAD_LIBCUSPATIAL=1
 else
     export UPLOAD_LIBCUSPATIAL=0
