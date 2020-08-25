ab7cd1456e27b067f6baf9d0151e629eabcfefb5
AJ Schmidt <ajschmidt8@users.noreply.github.com>
2020-08-25 11:13:24 -0400

    revert googlebench version change

--- cpp/cmake/Templates/GoogleBenchmark.CMakeLists.txt.cmake
+++ cpp/cmake/Templates/GoogleBenchmark.CMakeLists.txt.cmake
@@ -4,7 +4,7 @@ include(ExternalProject)
 
 ExternalProject_Add(GoogleBenchmark
                     GIT_REPOSITORY    https://github.com/google/benchmark.git
-                    GIT_TAG           v1.5.1 
+                    GIT_TAG           master
                     SOURCE_DIR        "${GBENCH_ROOT}/googlebenchmark"
                     BINARY_DIR        "${GBENCH_ROOT}/build"
                     INSTALL_DIR		    "${GBENCH_ROOT}/install"
