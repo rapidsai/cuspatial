aead9c5bf755605057b07bdeb960ee8733e38bf6
Wei Ji <weiji.leong@vuw.ac.nz>
2021-01-15 16:44:29 +1300

    Revert "Test cuspatial with gdal 3.1.4"
    This reverts commit a49e0ab81f7750e4cadbae3978fe1046abc6cf2b.

--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -43,11 +43,9 @@ conda activate rapids
 gpuci_conda_retry install "cudf=${MINOR_VERSION}.*" "cudatoolkit=$CUDA_REL" \
     "rapids-build-env=$MINOR_VERSION.*"
 
-# https://docs.rapids.ai/maintainers/depmgmt/
+# https://docs.rapids.ai/maintainers/depmgmt/ 
 # gpuci_conda_retry remove --force rapids-build-env
 # gpuci_conda_retry install "your-pkg=1.0.0"
-gpuci_conda_retry remove --force rapids-build-env
-gpuci_conda_retry install "gdal=3.1.4"
 
 gpuci_logger "Check versions"
 python --version
