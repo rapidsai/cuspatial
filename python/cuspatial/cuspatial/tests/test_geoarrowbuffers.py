67be534db2ca3f7492848e034a38735a288c3b17
galipremsagar <sagarprem75@gmail.com>
2021-06-30 13:21:44 -0700

    fix imports

--- python/cuspatial/cuspatial/tests/test_geoarrowbuffers.py
+++ python/cuspatial/cuspatial/tests/test_geoarrowbuffers.py
@@ -12,7 +12,7 @@ from shapely.geometry import (
 )
 
 import cudf
-from cudf.tests.utils import assert_eq
+from cudf.testing._utils import assert_eq
 
 from cuspatial.geometry.geocolumn import GeoColumn
 from cuspatial import (
