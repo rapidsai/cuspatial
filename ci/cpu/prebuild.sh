77971ac91a24228bc46cf461c0ac7b6f2ed78e44
Ray Douglass <3107146+raydouglass@users.noreply.github.com>
2022-01-27 11:27:51 -0500

    Always upload cuspatial packages (#481)
    Always upload cuspatial packages
    
    Authors:
      - Ray Douglass (https://github.com/raydouglass)
    
    Approvers:
      - AJ Schmidt (https://github.com/ajschmidt8)
      - Jordan Jacobelli (https://github.com/Ethyling)
    
    URL: https://github.com/rapidsai/cuspatial/pull/481

--- ci/cpu/prebuild.sh
+++ ci/cpu/prebuild.sh
@@ -1,21 +1,7 @@
 #!/usr/bin/env bash
 
-DEFAULT_CUDA_VER="11.5"
-DEFAULT_PYTHON_VER="3.8"
-
-#Upload cuspatial once per PYTHON
-if [[ "$CUDA" == "${DEFAULT_CUDA_VER}" ]]; then
-    export UPLOAD_CUSPATIAL=1
-else
-    export UPLOAD_CUSPATIAL=0
-fi
-
-#Upload libcuspatial once per CUDA
-if [[ "$PYTHON" == "${DEFAULT_PYTHON_VER}" ]]; then
-    export UPLOAD_LIBCUSPATIAL=1
-else
-    export UPLOAD_LIBCUSPATIAL=0
-fi
+export UPLOAD_CUSPATIAL=1
+export UPLOAD_LIBCUSPATIAL=1
 
 if [[ -z "$PROJECT_FLASH" || "$PROJECT_FLASH" == "0" ]]; then
     #If project flash is not activate, always build both
