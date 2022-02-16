0d954e016021a857142088dfa191aaf0627b5d86
Mark Harris <mharris@nvidia.com>
2022-02-17 09:45:05 +1100

    Remove unused macro.

--- cpp/include/cuspatial/types.hpp
+++ cpp/include/cuspatial/types.hpp
@@ -16,12 +16,6 @@
 
 #pragma once
 
-#ifdef __CUDACC__
-#define CUSPATIAL_HOST_DEVICE __host__ __device__
-#else
-#define CUSPATIAL_HOST_DEVICE
-#endif
-
 #include <cstdint>
 
 namespace cuspatial {
