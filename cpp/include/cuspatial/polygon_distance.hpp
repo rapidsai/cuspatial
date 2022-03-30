e52d1de29363bc6a4e4314f871ce92a7f81d207b
Michael Wang <michaelwang0905@icloud.com>
2022-03-29 23:17:53 -0700

    Updating API to accept pairwise input

--- cpp/include/cuspatial/polygon_distance.hpp
+++ cpp/include/cuspatial/polygon_distance.hpp
@@ -22,20 +22,40 @@
 namespace cuspatial {
 
 /**
- * @brief Compute the shortest distance between every pair of spaces.
+ * @brief Compute the shortest distance between pairs of polygons.
  *
- * @param [in] xs The x-coordinates of input points
- * @param [in] ys The y-coordinates of input points
- * @param [in] space_offsets The indices that marks the start of each space, plus the end index.
- * @param [in] mr Memory resource that's used to allocate result column.
+ * The shortest distance between two polygons is computed with the shortest distance
+ * between every pair of vertex, line segments in two polygons. If two polygons intersects,
+ * or one polygon contains another, the distance is 0.
+ *
+ * @param [in] poly1_offsets The indices that indicates the start coordinate of the polygon
+ * @param [in] poly1_ring_offsets The indices that indicates the start coordinate of the polygon
+ * @param [in] poly1_xs The x-coordinates of input points
+ * @param [in] poly1_ys The y-coordinates of input points
+ * @param [in] poly2_offsets The indices that indicates the start coordinate of the polygon
+ * @param [in] poly2_ring_offsets The indices that indicates the start coordinate of the polygon
+ * @param [in] poly2_xs The x-coordinates of input points
+ * @param [in] poly2_ys The y-coordinates of input points
+ * @param [in] mr Memory resource allocating memory to store the result column
+ *
+ * @throws cuspatial::logic_error if the poly1_offsets != poly2_offsets, unmatching number of
+ * polygons in polygon list 1 and polygon list 2.
+ * @throws cuspatial::logic_error if any of poly1_xs, poly1_ys, poly2_xs, poly2_ys have different
+ * types.
+ * @throws cuspatial::logic_error if any of poly1_xs, poly1_ys, poly2_xs, poly2_ys contains null.
  *
  * @return A cudf::column that contains the shortest distances
  */
 
-std::unique_ptr<cudf::column> polygon_distance(
-  cudf::column_view const& xs,
-  cudf::column_view const& ys,
-  cudf::device_span<cudf::size_type> const& space_offsets,
+std::unique_ptr<cudf::column> pairwise_polygon_distance(
+  cudf::device_span<cudf::size_type> const& poly1_offsets,
+  cudf::device_span<cudf::size_type> const& poly1_ring_offsets,
+  cudf::column_view const& poly1_xs,
+  cudf::column_view const& poly1_ys,
+  cudf::device_span<cudf::size_type> const& poly2_offsets,
+  cudf::device_span<cudf::size_type> const& poly2_ring_offsets,
+  cudf::column_view const& poly2_xs,
+  cudf::column_view const& poly2_ys,
   rmm::mr::device_memory_resource* mr = rmm::mr::get_current_device_resource());
 
 }  // namespace cuspatial
--- cpp/src/spatial/polygon_distance.cu
+++ cpp/src/spatial/polygon_distance.cu
@@ -1,4 +1,18 @@
-#include "distances_utilities.cuh"
+/*
+ * Copyright (c) 2022, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required point_b_y applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #include <cuspatial/error.hpp>
 #include <cuspatial/polygon_distance.hpp>
@@ -15,47 +29,32 @@
 
 namespace {
 
+using size_type = cudf::size_type;
+
+/**
+ * @brief Device function to compute the distance between polygons
+ *
+ * The first step is to compute if two polygons intersects or constains
+ * one another. If so, the distance is 0. Otherwise, compute every pair
+ * of line segment-line segment, point-line segment and point-point for
+ * shortest distance.
+ *
+ */
 struct polygon_distance_functor {
   template <typename T>
   std::enable_if_t<std::is_floating_point<T>::value, std::unique_ptr<cudf::column>> operator()(
-    cudf::column_view const& xs,
-    cudf::column_view const& ys,
-    cudf::device_span<cudf::size_type> const& space_offsets,
+    cudf::device_span<cudf::size_type> const& poly1_offsets,
+    cudf::device_span<cudf::size_type> const& poly1_ring_offsets,
+    cudf::column_view const& poly1_xs,
+    cudf::column_view const& poly1_ys,
+    cudf::device_span<cudf::size_type> const& poly2_offsets,
+    cudf::device_span<cudf::size_type> const& poly2_ring_offsets,
+    cudf::column_view const& poly2_xs,
+    cudf::column_view const& poly2_ys,
     rmm::cuda_stream_view stream,
     rmm::mr::device_memory_resource* mr)
   {
-    auto const num_points = static_cast<uint32_t>(xs.size());
-    auto const num_spaces = static_cast<uint32_t>(space_offsets.size());
-
-    CUSPATIAL_EXPECTS(num_spaces < (1 << 15), "Total number of spaces must be less than 2^16");
-
-    auto const num_results = num_spaces * num_spaces;
-
-    auto result =
-      cudf::make_fixed_width_column(cudf::data_type{cudf::type_id{cudf::type_to_id<T>()}},
-                                    num_results,
-                                    cudf::mask_state::UNALLOCATED,
-                                    stream,
-                                    mr);
-
-    auto result_mview = result->mutable_view();
-    thrust::fill(rmm::exec_policy(stream),
-                 result_mview.begin<T>(),
-                 result_mview.end<T>(),
-                 std::numeric_limits<T>::max());
-
-    auto const threads_per_block = 64;
-    auto const num_blocks        = (num_points + threads_per_block - 1) / threads_per_block;
-
-    auto kernel = cuspatial::detail::distances_kernel<T, cuspatial::DISTANCE_KIND::SHORTEST>;
-    kernel<<<num_blocks, threads_per_block>>>(num_points,
-                                              xs.begin<T>(),
-                                              ys.begin<T>(),
-                                              space_offsets.size(),
-                                              space_offsets.begin(),
-                                              result_mview.begin<T>());
-
-    return result;
+    // TODO
   }
 
   template <typename T, typename... Args>
@@ -70,24 +69,53 @@ struct polygon_distance_functor {
 
 namespace cuspatial {
 
-std::unique_ptr<cudf::column> polygon_distance(
-  cudf::column_view const& xs,
-  cudf::column_view const& ys,
-  cudf::device_span<cudf::size_type> const& space_offsets,
+std::unique_ptr<cudf::column> pairwise_polygon_distance(
+  cudf::device_span<cudf::size_type> const& poly1_offsets,
+  cudf::device_span<cudf::size_type> const& poly1_ring_offsets,
+  cudf::column_view const& poly1_xs,
+  cudf::column_view const& poly1_ys,
+  cudf::device_span<cudf::size_type> const& poly2_offsets,
+  cudf::device_span<cudf::size_type> const& poly2_ring_offsets,
+  cudf::column_view const& poly2_xs,
+  cudf::column_view const& poly2_ys,
   rmm::mr::device_memory_resource* mr)
 {
   using device_span_size_type = cudf::device_span<cudf::size_type>::size_type;
 
-  CUSPATIAL_EXPECTS(xs.type() == ys.type(), "Inputs `xs` and `ys` must have same type.");
-  CUSPATIAL_EXPECTS(xs.size() == ys.size(), "Inputs `xs` and `ys` must have same length.");
+  CUSPATIAL_EXPECTS(poly1_xs.type() == poly1_ys.type(),
+                    "Inputs `poly1_xs` and `poly1_ys` must have same type.");
+  CUSPATIAL_EXPECTS(poly2_xs.type() == poly2_ys.type(),
+                    "Inputs `poly2_xs` and `poly2_ys` must have same type.");
+  CUSPATIAL_EXPECTS(poly1_xs.type() == poly2_ys.type(),
+                    "Polygons of the same pair must have the same type.");
+  CUSPATIAL_EXPECTS(poly1_xs.size() == poly1_ys.size(),
+                    "Inputs `xs` and `ys` must have same length.");
+  CUSPATIAL_EXPECTS(poly2_xs.size() == poly2_ys.size(),
+                    "Inputs `xs` and `ys` must have same length.");
 
-  CUSPATIAL_EXPECTS(not xs.has_nulls() and not ys.has_nulls(), "Inputs must not have nulls.");
+  CUSPATIAL_EXPECTS(not poly1_xs.has_nulls() and not poly1_ys.has_nulls() and
+                      not poly2_xs.has_nulls() and not poly2_ys.has_nulls(),
+                    "Coordinates must not have nulls.");
 
-  CUSPATIAL_EXPECTS(static_cast<device_span_size_type>(xs.size()) >= space_offsets.size(),
-                    "At least one point is required for each space");
+  CUSPATIAL_EXPECTS(
+    static_cast<device_span_size_type>(poly1_xs.size()) >= poly1_ring_offsets.size(),
+    "At least 1 point is required for each linear ring of the polygon.");
+  CUSPATIAL_EXPECTS(
+    static_cast<device_span_size_type>(poly2_xs.size()) >= poly2_ring_offsets.size(),
+    "At least 1 point is required for each linear ring of the polygon.");
 
-  return cudf::type_dispatcher(
-    xs.type(), polygon_distance_functor{}, xs, ys, space_offsets, rmm::cuda_stream_default, mr);
+  return cudf::type_dispatcher(poly1_xs.type(),
+                               polygon_distance_functor{},
+                               poly1_offsets,
+                               poly1_ring_offsets,
+                               poly1_xs,
+                               poly1_ys,
+                               poly2_offsets,
+                               poly2_ring_offsets,
+                               poly2_xs,
+                               poly2_ys,
+                               rmm::cuda_stream_default,
+                               mr);
 }
 
 }  // namespace cuspatial
