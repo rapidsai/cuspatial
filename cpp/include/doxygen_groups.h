ed3b5f272e92c16f32ef498c9cb2aa374f7c4202
Michael Wang <isVoid@users.noreply.github.com>
2022-05-23 14:16:17 -0700

    Add cuspatial groupings

---
+++ cpp/include/doxygen_groups.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2022, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file
+ * @brief Doxygen group definitions
+ */
+
+// This header is only processed by doxygen and does
+// not need to be included in any source file.
+// Below are the main groups that doxygen uses to build
+// the Modules page in the specified order.
+//
+// To add a new API to an existing group, just use the
+// @ingroup tag to the API's doxygen comment.
+// Add a new group by first specifying in the hierarchy below.
+
+/**
+ * @defgroup cuspatial_constants Constants
+ * @defgroup spatial_api Spatial APIs
+ * @{
+ *      @defgroup coordinate_transform Coordinate Transform
+ *      @defgroup distance Distance
+ *      @defgroup spatial_relationship Spatial Relationship
+ *      @defgroup cubic_spline Cubic Spline
+ * @}
+ * @defgroup trajectory_api Trajectory APIs
+ * @defgroup spatial_indexing Spatial Indexing
+ * @defgroup spatial_join Spatial Join
+ * @defgroup cuspatial_types Types
+ * @{
+ *      @defgroup type_definition_operator Definition and Operators
+ *      @defgroup type_factories Factory Methods
+ * @}
+ * @defgroup io I/O
+ * @defgroup exception Exception
+ */
