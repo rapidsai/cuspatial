d27ff3e8fdbf4d36d892066f83d1b2d5873f00b8
Michael Wang <isVoid@users.noreply.github.com>
2022-05-26 15:42:45 -0700

    doc

--- cpp/include/cuspatial/distances/linestring_distance.hpp
+++ cpp/include/cuspatial/distances/linestring_distance.hpp
@@ -84,7 +84,7 @@ namespace cuspatial {
  * @param linestring1_points_y y-component of points in the first linestring of each pair.
  * @param linestring2_offsets Indices of the first point of the second linestring of each pair.
  * @param linestring2_points_x x-component of points in the second linestring of each pair.
- * @param linestring2_points_y y-component of points in the secondlinestring of each pair.
+ * @param linestring2_points_y y-component of points in the second linestring of each pair.
  * @param mr Device memory resource used to allocate the returned column's device memory.
  * @return A column of shortest distances between each pair of linestrings.
  *
