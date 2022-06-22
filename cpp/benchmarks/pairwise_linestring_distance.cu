bf182ea8192b583df728f2fda38d4312934a0a52
Michael Wang <isVoid@users.noreply.github.com>
2022-06-22 12:22:49 -0700

    style

--- cpp/benchmarks/pairwise_linestring_distance.cu
+++ cpp/benchmarks/pairwise_linestring_distance.cu
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 #include <benchmarks/fixture/rmm_pool_raii.hpp>
 #include <nvbench/nvbench.cuh>
 
