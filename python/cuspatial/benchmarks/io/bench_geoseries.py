ac1f525f23f843d4a1554af08e4315a6216e7ec9
H. Thomson Comer <thomcom@gmail.com>
2022-07-25 13:23:33 -0500

    First pass of four df size benchmarks.

---
+++ python/cuspatial/benchmarks/io/bench_geoseries.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2022, NVIDIA CORPORATION.
+
+"""Benchmarks of GeoSeries methods."""
+
+import pytest
+from config import cuspatial, cupy
+from utils import benchmark_with_object
+
+
+def bench_from_geoseries_100(benchmark, gpdf_100):
+    benchmark(cuspatial.from_geopandas, gpdf_100["geometry"])
+
+
+def bench_from_geoseries_1000(benchmark, gpdf_1000):
+    benchmark(cuspatial.from_geopandas, gpdf_1000["geometry"])
+
+
+def bench_from_geoseries_10000(benchmark, gpdf_10000):
+    benchmark(cuspatial.from_geopandas, gpdf_10000["geometry"])
+
+
+def bench_from_geoseries_100000(benchmark, gpdf_100000):
+    benchmark(cuspatial.from_geopandas, gpdf_100000["geometry"])
