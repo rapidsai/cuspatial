0700863f4e6fc5bf949b1b633f37a72aad13f654
Michael Wang <isVoid@users.noreply.github.com>
2022-08-15 13:01:12 -0700

    Move trajectory into sub-package

---
+++ python/cuspatial/cuspatial/core/trajectory/__init__.py
@@ -0,0 +1,11 @@
+from .generic import (
+    derive_trajectories,
+    trajectory_bounding_boxes,
+    trajectory_distances_and_speeds,
+)
+
+__all__ = [
+    "derive_trajectories",
+    "trajectory_bounding_boxes",
+    "trajectory_distances_and_speeds",
+]
