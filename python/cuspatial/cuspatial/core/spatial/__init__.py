e0845f28a12beaec13b70efbbf1c7b93f40febc9
Michael Wang <isVoid@users.noreply.github.com>
2022-08-16 19:06:36 -0700

    Update top level module visibility

--- python/cuspatial/cuspatial/core/spatial/__init__.py
+++ python/cuspatial/cuspatial/core/spatial/__init__.py
@@ -1,26 +1,26 @@
+from .bound import (
+    polygon_bounding_boxes,
+    polyline_bounding_boxes,
+)
+from .filter import points_in_spatial_window
+
+from .index import quadtree_on_points
+
+from .join import (
+    point_in_polygon,
+    join_quadtree_and_bounding_boxes,
+    quadtree_point_in_polygon,
+    quadtree_point_to_nearest_polyline
+)
+
 from .measure import (
     directed_hausdorff_distance,
     haversine_distance,
     pairwise_linestring_distance
 )
 
-from .bound import (
-    polygon_bounding_boxes,
-    polyline_bounding_boxes,
-)
-
 from .project import (
     lonlat_to_cartesian,
-)
-
-from .filter import points_in_spatial_window
-
-from .index import quadtree_on_points
-
-from .join import (
-    join_quadtree_and_bounding_boxes,
-    quadtree_point_in_polygon,
-    quadtree_point_to_nearest_polyline
 )
 
 __all__ = [
@@ -31,6 +31,7 @@ __all__ = [
     "pairwise_linestring_distance",
     "polygon_bounding_boxes",
     "polyline_bounding_boxes",
+    "point_in_polygon",
     "points_in_spatial_window",
     "quadtree_on_points",
     "quadtree_point_in_polygon",
