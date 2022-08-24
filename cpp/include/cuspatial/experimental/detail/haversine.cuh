9fe19377570a1ca80844bf2335bafe56e3dd4d2d
Mark Harris <mharris@nvidia.com>
2022-08-24 21:52:41 +1000

    Fix haversine (and simplify static_asserts)

--- cpp/include/cuspatial/experimental/detail/haversine.cuh
+++ cpp/include/cuspatial/experimental/detail/haversine.cuh
@@ -58,7 +58,7 @@ struct haversine_distance_functor {
 
 }  // namespace detail
 
-template <class LonLatItA, class LonLatItB, class OutputIt, class Location, class T>
+template <class LonLatItA, class LonLatItB, class OutputIt, class T>
 OutputIt haversine_distance(LonLatItA a_lonlat_first,
                             LonLatItA a_lonlat_last,
                             LonLatItB b_lonlat_first,
@@ -66,15 +66,14 @@ OutputIt haversine_distance(LonLatItA a_lonlat_first,
                             T const radius,
                             rmm::cuda_stream_view stream)
 {
-  using LocationA = detail::iterator_value_type<LonLatItA>;
-  using LocationB = detail::iterator_value_type<LonLatItB>;
-  static_assert(detail::is_same<vec_2d<T>, LocationA, LocationB>(),
+  static_assert(detail::is_same<vec_2d<T>,
+                                detail::iterator_value_type<LonLatItA>,
+                                detail::iterator_value_type<LonLatItB>>(),
                 "Inputs must be cuspatial::vec_2d");
   static_assert(
     detail::is_same_floating_point<T,
-                                   typename LocationA::value_type,
-                                   typename LocationB::value_type,
-                                   typename std::iterator_traits<OutputIt>::value_type>(),
+                                   typename detail::iterator_vec_base_type<LonLatItA>,
+                                   typename detail::iterator_value_type<OutputIt>>(),
     "All iterator types and radius must have the same floating-point coordinate value type.");
 
   CUSPATIAL_EXPECTS(radius > 0, "radius must be positive.");
