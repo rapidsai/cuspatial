50b54699d642b054c5845c7df28fa6be44a0ee1d
H. Thomson Comer <thomcom@gmail.com>
2022-08-29 11:37:58 -0500

    Add reversed tests to test_shapefile_reader.py

--- python/cuspatial/cuspatial/tests/test_shapefile_reader.py
+++ python/cuspatial/cuspatial/tests/test_shapefile_reader.py
@@ -9,6 +9,8 @@ import cudf
 
 import cuspatial
 
+from cuspatial.io.shapefile import WindingOrder
+
 shapefiles_path = os.path.join(
     os.environ["CUSPATIAL_HOME"], "test_fixtures", "shapefiles"
 )
@@ -42,6 +44,27 @@ def test_zero_polygons():
     )
 
 
+def test_one_polygon_reversed():
+    f_pos, r_pos, points = cuspatial.read_polygon_shapefile(
+        os.path.join(shapefiles_path, "one_poly.shp"),
+        outer_ring_order=WindingOrder.CLOCKWISE
+    )
+    cudf.testing.assert_series_equal(
+        f_pos, cudf.Series([0], dtype=np.int32, name="f_pos")
+    )
+    cudf.testing.assert_series_equal(
+        r_pos, cudf.Series([0], dtype=np.int32, name="r_pos")
+    )
+    cudf.testing.assert_frame_equal(
+        points,
+        cudf.DataFrame(
+            {
+                "x": cudf.Series([-10, 5, 5, -10, -10], dtype=np.float64),
+                "y": cudf.Series([-10, -10, 5, 5, -10], dtype=np.float64),
+            }
+        ),
+    )
+
 def test_one_polygon():
     f_pos, r_pos, points = cuspatial.read_polygon_shapefile(
         os.path.join(shapefiles_path, "one_poly.shp")
@@ -58,6 +81,32 @@ def test_one_polygon():
             {
                 "x": cudf.Series([-10, -10, 5, 5, -10], dtype=np.float64),
                 "y": cudf.Series([-10, 5, 5, -10, -10], dtype=np.float64),
+            }
+        ),
+    )
+
+
+def test_two_polygons_reversed():
+    f_pos, r_pos, points = cuspatial.read_polygon_shapefile(
+        os.path.join(shapefiles_path, "two_polys.shp"),
+        outer_ring_order=WindingOrder.CLOCKWISE
+    )
+    cudf.testing.assert_series_equal(
+        f_pos, cudf.Series([0, 1], dtype=np.int32, name="f_pos")
+    )
+    cudf.testing.assert_series_equal(
+        r_pos, cudf.Series([0, 5], dtype=np.int32, name="r_pos")
+    )
+    cudf.testing.assert_frame_equal(
+        points,
+        cudf.DataFrame(
+            {
+                "x": cudf.Series(
+                    [-10, 5, 5, -10, -10, 0, 10, 10, 0, 0], dtype=np.float64
+                ),
+                "y": cudf.Series(
+                    [-10, -10, 5, 5, -10, 0, 0, 10, 10, 0], dtype=np.float64
+                ),
             }
         ),
     )
