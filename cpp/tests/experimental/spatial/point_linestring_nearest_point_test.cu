7e1dffed3c6216f23bfde2d38e887f101b37860a
Michael Wang <isVoid@users.noreply.github.com>
2022-09-08 14:15:13 -0700

    Adds simple test, vector of vec2d equivalence test utility

---
+++ cpp/tests/experimental/spatial/point_linestring_nearest_point_test.cu
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2022, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "../../utility/vector_equality.hpp"
+#include "cuspatial/experimental/point_distance.cuh"
+
+#include <cuspatial/detail/iterator.hpp>
+#include <cuspatial/error.hpp>
+#include <cuspatial/experimental/point_linestring_nearest_point.cuh>
+#include <cuspatial/experimental/type_utils.hpp>
+#include <cuspatial/vec_2d.hpp>
+
+#include <rmm/device_vector.hpp>
+
+#include <thrust/host_vector.h>
+#include <thrust/iterator/constant_iterator.h>
+#include <thrust/iterator/counting_iterator.h>
+
+#include <type_traits>
+
+namespace cuspatial {
+namespace test {
+
+template <typename T>
+struct PairwisePointLinestringNearestPointTest : public ::testing::Test {
+};
+
+// float and double are logically the same but would require seperate tests due to precision.
+using TestTypes = ::testing::Types<float, double>;
+TYPED_TEST_CASE(PairwisePointLinestringNearestPointTest, TestTypes);
+
+TYPED_TEST(PairwisePointLinestringNearestPointTest, Empty)
+{
+  using T       = TypeParam;
+  using CartVec = std::vector<vec_2d<T>>;
+
+  auto num_pairs          = 0;
+  auto points_geometry_it = thrust::make_counting_iterator(0);
+  auto points_it          = rmm::device_vector<vec_2d<T>>(CartVec{});
+
+  auto linestring_geometry_it  = thrust::make_counting_iterator(0);
+  auto linestring_part_offsets = rmm::device_vector<int32_t>(std::vector<int32_t>{0});
+  auto linestring_points_it    = rmm::device_vector<vec_2d<T>>(CartVec{});
+
+  auto nearest_linestring_parts_id   = rmm::device_vector<int32_t>(num_pairs);
+  auto nearest_linestring_segment_id = rmm::device_vector<int32_t>(num_pairs);
+  auto neartest_point_coordinate     = rmm::device_vector<vec_2d<T>>(num_pairs);
+
+  auto output_it =
+    thrust::make_zip_iterator(thrust::make_tuple(nearest_linestring_parts_id.begin(),
+                                                 nearest_linestring_segment_id.begin(),
+                                                 neartest_point_coordinate.begin()));
+
+  auto ret = pairwise_point_linestring_nearest_point(points_geometry_it,
+                                                     points_geometry_it + num_pairs + 1,
+                                                     points_it.begin(),
+                                                     points_it.end(),
+                                                     linestring_geometry_it,
+                                                     linestring_part_offsets.begin(),
+                                                     linestring_part_offsets.end(),
+                                                     linestring_points_it.begin(),
+                                                     linestring_points_it.end(),
+                                                     output_it);
+
+  EXPECT_EQ(nearest_linestring_parts_id, std::vector<int32_t>{});
+  EXPECT_EQ(nearest_linestring_segment_id, std::vector<int32_t>{});
+  expect_vec2d_vector_equivalent(thrust::host_vector<vec_2d<T>>(neartest_point_coordinate),
+                                 std::vector<vec_2d<T>>{});
+  EXPECT_EQ(std::distance(output_it, ret), num_pairs);
+}
+
+}  // namespace test
+}  // namespace cuspatial
--- cpp/tests/utility/vector_equality.hpp
+++ cpp/tests/utility/vector_equality.hpp
@@ -16,13 +16,18 @@
 
 #pragma once
 
+#include <cuspatial/vec_2d.hpp>
+
+#include <thrust/iterator/zip_iterator.h>
+
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 #include <type_traits>
-
 namespace cuspatial {
 namespace test {
+
+using namespace testing;
 
 template <typename Vector1, typename Vector2>
 inline void expect_vector_equivalent(Vector1 lhs, Vector2 rhs)
@@ -31,10 +36,41 @@ inline void expect_vector_equivalent(Vector1 lhs, Vector2 rhs)
   static_assert(std::is_same_v<T, typename Vector2::value_type>, "Mismatch value type.");
 
   if constexpr (std::is_same_v<T, float>) {
-    EXPECT_THAT(lhs, ::testing::Pointwise(::testing::FloatEq(), rhs));
+    EXPECT_THAT(lhs, Pointwise(FloatEq(), rhs));
   } else {
-    EXPECT_THAT(lhs, ::testing::Pointwise(::testing::DoubleEq(), rhs));
+    EXPECT_THAT(lhs, Pointwise(DoubleEq(), rhs));
   }
+}
+
+template <typename Vec2d>
+auto vec2d_equivalant(Vec2d v)
+{
+  using T = typename Vec2d::value_type;
+  if constexpr (std::is_same_v<T, float>) {
+    return AllOf(Field(&Vec2d::x, FloatEq(v.x)), Field(&Vec2d::y, FloatEq(v.y)));
+  } else {
+    return AllOf(Field(&Vec2d::x, DoubleEq(v.x)), Field(&Vec2d::y, DoubleEq(v.y)));
+  }
+}
+
+template <typename Vector1, typename Vector2>
+inline void expect_vec2d_vector_equivalent(Vector1 lhs, Vector2 rhs)
+{
+  using Vec2d = typename Vector1::value_type;
+  using T     = typename Vec2d::value_type;
+
+  static_assert(std::is_same_v<Vec2d, typename Vector2::value_type>, "Mismatch value type.");
+  static_assert(std::is_same_v<Vec2d, vec_2d<T>>, "Must be cuspatial::vec2d vectors.");
+
+  EXPECT_TRUE(lhs.size() == rhs.size());
+
+  auto begin = thrust::make_zip_iterator(thrust::make_tuple(lhs.begin(), rhs.begin()));
+
+  std::for_each(begin, begin + lhs.size(), [](auto t) {
+    auto lhs = thrust::get<0>(t);
+    auto rhs = thrust::get<1>(t);
+    EXPECT_THAT(lhs, vec2d_equivalant(rhs));
+  });
 }
 
 }  // namespace test
