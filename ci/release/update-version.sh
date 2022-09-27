93c1eea0fa43837eb4b5430a5fec2124984fd50b
Mark Harris <mharris@nvidia.com>
2022-09-27 15:00:41 +1000

    Fix update_version.sh

--- ci/release/update-version.sh
+++ ci/release/update-version.sh
@@ -47,5 +47,5 @@ for FILE in conda/environments/*.yml; do
 done
 
 # Doxyfile update
-sed_runner "s|\(TAGFILES.*librmm/\).*|\1${NEXT_SHORT_TAG}|" cpp/doxygen/Doxyfile
-sed_runner "s|\(TAGFILES.*libcudf/\).*|\1${NEXT_SHORT_TAG}|" cpp/doxygen/Doxyfile
+sed_runner "/PROJECT_NUMBER/ s|[0-9]\+.[0-9]\+|${NEXT_SHORT_TAG}|g" cpp/doxygen/Doxyfile
+sed_runner "/TAGFILES/ s|[0-9]\+.[0-9]\+|${NEXT_SHORT_TAG}|g" cpp/doxygen/Doxyfile
