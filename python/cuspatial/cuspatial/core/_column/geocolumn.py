2532921d64b06db3699c0af1d149757d832e62c7
Thomson Comer <thomcom@gmail.com>
2022-09-30 09:19:14 -0500

    Remove memory_usage as it has gotten confusing.

--- python/cuspatial/cuspatial/core/_column/geocolumn.py
+++ python/cuspatial/cuspatial/core/_column/geocolumn.py
@@ -1,5 +1,4 @@
 # Copyright (c) 2021-2022 NVIDIA CORPORATION
-from functools import cached_property
 from typing import Tuple, TypeVar
 
 import cupy as cp
@@ -222,16 +221,3 @@ class GeoColumn(ColumnBase):
             ),
             meta,
         )
-
-    @cached_property
-    def memory_usage(self) -> int:
-        """
-        Outputs how much memory is used by the underlying geometries.
-        """
-        final_size = self._meta.input_types.memory_usage()
-        final_size = final_size + self._meta.union_offsets.memory_usage()
-        final_size = final_size + self.points._column.memory_usage
-        final_size = final_size + self.mpoints._column.memory_usage
-        final_size = final_size + self.lines._column.memory_usage
-        final_size = final_size + self.polygons._column.memory_usage
-        return final_size
