37d3db570c52308468a6740ef3227717afc5b256
Paul Taylor <paul.e.taylor@me.com>
2022-10-05 14:13:54 -0700

    Add header only `cuspatial::quadtree_on_points` (#639)
    Closes https://github.com/rapidsai/cuspatial/issues/566.
    
    Authors:
      - Paul Taylor (https://github.com/trxcllnt)
    
    Approvers:
      - Mark Harris (https://github.com/harrism)
      - Michael Wang (https://github.com/isVoid)
    
    URL: https://github.com/rapidsai/cuspatial/pull/639

--- cpp/src/join/detail/traversal.cuh
+++ cpp/src/join/detail/traversal.cuh
@@ -17,7 +17,8 @@
 #pragma once
 
 #include <indexing/construction/detail/utilities.cuh>
-#include <utility/z_order.cuh>
+
+#include <cuspatial/detail/utility/z_order.cuh>
 
 #include <rmm/cuda_stream_view.hpp>
 #include <rmm/device_uvector.hpp>
