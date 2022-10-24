121e14652f9c1440f7479ae25a248efd59190e3d
Thomson Comer <thomcom@gmail.com>
2022-10-24 12:34:53 -0500

    Create new branch with pairwise_point_in_polygon cpp changes.

--- cpp/include/cuspatial/experimental/detail/pairwise_point_in_polygon.cuh
+++ cpp/include/cuspatial/experimental/detail/pairwise_point_in_polygon.cuh
@@ -40,45 +40,33 @@ template <class Cart2dItA,
           class Cart2dItBDiffType = typename std::iterator_traits<Cart2dItB>::difference_type,
           class OffsetItADiffType = typename std::iterator_traits<OffsetIteratorA>::difference_type,
           class OffsetItBDiffType = typename std::iterator_traits<OffsetIteratorB>::difference_type>
-__global__ void point_in_polygon_kernel(Cart2dItA test_points_first,
-                                        Cart2dItADiffType const num_test_points,
-                                        OffsetIteratorA poly_offsets_first,
-                                        OffsetItADiffType const num_polys,
-                                        OffsetIteratorB ring_offsets_first,
-                                        OffsetItBDiffType const num_rings,
-                                        Cart2dItB poly_points_first,
-                                        Cart2dItBDiffType const num_poly_points,
-                                        OutputIt result)
+__global__ void pairwise_point_in_polygon_kernel(Cart2dItA test_points_first,
+                                                 Cart2dItADiffType const num_test_points,
+                                                 OffsetIteratorA poly_offsets_first,
+                                                 OffsetItADiffType const num_polys,
+                                                 OffsetIteratorB ring_offsets_first,
+                                                 OffsetItBDiffType const num_rings,
+                                                 Cart2dItB poly_points_first,
+                                                 Cart2dItBDiffType const num_poly_points,
+                                                 OutputIt result)
 {
   using Cart2d     = iterator_value_type<Cart2dItA>;
   using OffsetType = iterator_value_type<OffsetIteratorA>;
-
-  auto idx = blockIdx.x * blockDim.x + threadIdx.x;
-
+  auto idx         = blockIdx.x * blockDim.x + threadIdx.x;
   if (idx >= num_test_points) { return; }
 
-  int32_t hit_mask = 0;
-
   Cart2d const test_point = test_points_first[idx];
-
-  // for each polygon
-  for (auto poly_idx = 0; poly_idx < num_polys; poly_idx++) {
-    auto poly_idx_next    = poly_idx + 1;
-    OffsetType poly_begin = poly_offsets_first[poly_idx];
-    OffsetType poly_end =
-      (poly_idx_next < num_polys) ? poly_offsets_first[poly_idx_next] : num_rings;
-
-    bool const point_is_within = is_point_in_polygon(test_point,
-                                                     poly_begin,
-                                                     poly_end,
-                                                     ring_offsets_first,
-                                                     num_rings,
-                                                     poly_points_first,
-                                                     num_poly_points);
-
-    hit_mask |= point_is_within << poly_idx;
-  }
-  result[idx] = hit_mask;
+  // for the matching polygon
+  OffsetType poly_begin      = poly_offsets_first[idx];
+  OffsetType poly_end        = (idx + 1 < num_polys) ? poly_offsets_first[idx + 1] : num_rings;
+  bool const point_is_within = is_point_in_polygon(test_point,
+                                                   poly_begin,
+                                                   poly_end,
+                                                   ring_offsets_first,
+                                                   num_rings,
+                                                   poly_points_first,
+                                                   num_poly_points);
+  result[idx]                = point_is_within;
 }
 
 }  // namespace detail
@@ -88,16 +76,16 @@ template <class Cart2dItA,
           class OffsetIteratorA,
           class OffsetIteratorB,
           class OutputIt>
-OutputIt point_in_polygon(Cart2dItA test_points_first,
-                          Cart2dItA test_points_last,
-                          OffsetIteratorA polygon_offsets_first,
-                          OffsetIteratorA polygon_offsets_last,
-                          OffsetIteratorB poly_ring_offsets_first,
-                          OffsetIteratorB poly_ring_offsets_last,
-                          Cart2dItB polygon_points_first,
-                          Cart2dItB polygon_points_last,
-                          OutputIt output,
-                          rmm::cuda_stream_view stream)
+OutputIt pairwise_point_in_polygon(Cart2dItA test_points_first,
+                                   Cart2dItA test_points_last,
+                                   OffsetIteratorA polygon_offsets_first,
+                                   OffsetIteratorA polygon_offsets_last,
+                                   OffsetIteratorB poly_ring_offsets_first,
+                                   OffsetIteratorB poly_ring_offsets_last,
+                                   Cart2dItB polygon_points_first,
+                                   Cart2dItB polygon_points_last,
+                                   OutputIt output,
+                                   rmm::cuda_stream_view stream)
 {
   using T = iterator_vec_base_type<Cart2dItA>;
 
@@ -119,11 +107,11 @@ OutputIt point_in_polygon(Cart2dItA test_points_first,
   static_assert(std::is_same_v<iterator_value_type<OutputIt>, int32_t>,
                 "OutputIt must point to 32 bit integer type.");
 
-  CUSPATIAL_EXPECTS(num_polys <= std::numeric_limits<int32_t>::digits,
-                    "Number of polygons cannot exceed 31");
-
   CUSPATIAL_EXPECTS(num_rings >= num_polys, "Each polygon must have at least one ring");
   CUSPATIAL_EXPECTS(num_poly_points >= num_polys * 4, "Each ring must have at least four vertices");
+
+  CUSPATIAL_EXPECTS(num_test_points == num_polys,
+                    "Must pass in an equal number of points and polygons");
 
   // TODO: introduce a validation function that checks the rings of the polygon are
   // actually closed. (i.e. the first and last vertices are the same)
@@ -131,7 +119,7 @@ OutputIt point_in_polygon(Cart2dItA test_points_first,
   auto constexpr block_size = 256;
   auto const num_blocks     = (num_test_points + block_size - 1) / block_size;
 
-  detail::point_in_polygon_kernel<<<num_blocks, block_size, 0, stream.value()>>>(
+  detail::pairwise_point_in_polygon_kernel<<<num_blocks, block_size, 0, stream.value()>>>(
     test_points_first,
     num_test_points,
     polygon_offsets_first,
