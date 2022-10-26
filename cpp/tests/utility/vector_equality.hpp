eda8a36a91d5bb9f405987c2c5a26da5f1c1f3b6
Michael Wang <isVoid@users.noreply.github.com>
2022-10-25 17:14:48 -0700

    introduce near equal utility

--- cpp/tests/utility/vector_equality.hpp
+++ cpp/tests/utility/vector_equality.hpp
@@ -16,6 +16,7 @@
 
 #pragma once
 
+#include <cmath>
 #include <cuspatial/traits.hpp>
 #include <cuspatial/vec_2d.hpp>
 
@@ -42,6 +43,16 @@ auto floating_eq(T val)
   }
 }
 
+template <typename T>
+auto floating_near(T val, T abs_error)
+{
+  if constexpr (std::is_same_v<T, float>) {
+    return ::testing::FloatNear(val, abs_error);
+  } else {
+    return ::testing::FloatNear(val, abs_error);
+  }
+}
+
 MATCHER(vec_2d_matcher,
         std::string(negation ? "are not" : "are") + " approximately equal vec_2d structs")
 {
@@ -57,12 +68,42 @@ MATCHER(vec_2d_matcher,
   return false;
 }
 
+MATCHER_P(vec_2d_near_matcher,
+          abs_error,
+          std::string(negation ? "are not" : "are") + " approximately equal vec_2d structs")
+{
+  auto lhs = std::get<0>(arg);
+  auto rhs = std::get<1>(arg);
+
+  if (::testing::Matches(floating_near(rhs.x, abs_error))(lhs.x) &&
+      ::testing::Matches(floating_near(rhs.y, abs_error))(lhs.y))
+    return true;
+
+  *result_listener << lhs << " != " << rhs;
+
+  return false;
+}
+
 MATCHER(float_matcher, std::string(negation ? "are not" : "are") + " approximately equal floats")
 {
   auto lhs = std::get<0>(arg);
   auto rhs = std::get<1>(arg);
 
   if (::testing::Matches(floating_eq(rhs))(lhs)) return true;
+
+  *result_listener << std::setprecision(18) << lhs << " != " << rhs;
+
+  return false;
+}
+
+MATCHER_P(float_near_matcher,
+          abs_error,
+          std::string(negation ? "are not" : "are") + " approximately equal floats")
+{
+  auto lhs = std::get<0>(arg);
+  auto rhs = std::get<1>(arg);
+
+  if (::testing::Matches(floating_near(rhs, abs_error))(lhs)) return true;
 
   *result_listener << std::setprecision(18) << lhs << " != " << rhs;
 
@@ -103,10 +144,27 @@ inline void expect_vector_equivalent(Vector1 const& lhs, Vector2 const& rhs)
   }
 }
 
-#define CUSPATIAL_EXPECT_VECTORS_EQUIVALENT(lhs, rhs...) \
-  do {                                                   \
-    SCOPED_TRACE(" <--  line of failure\n");             \
-    expect_vector_equivalent(lhs, rhs);                  \
+template <typename Vector1, typename Vector2, typename T = typename Vector1::value_type>
+inline void expect_vector_equivalent(Vector1 const& lhs, Vector2 const& rhs, T abs_error)
+{
+  static_assert(std::is_same_v<T, typename Vector2::value_type>, "Value type mismatch.");
+  static_assert(!std::is_integral_v<T>, "Integral types cannot be compared with an error.");
+
+  if constexpr (cuspatial::is_vec_2d<T>()) {
+    EXPECT_THAT(to_host<T>(lhs),
+                ::testing::Pointwise(vec_2d_near_matcher(abs_error), to_host<T>(rhs)));
+  } else if constexpr (std::is_floating_point_v<T>) {
+    EXPECT_THAT(to_host<T>(lhs),
+                ::testing::Pointwise(float_near_matcher(abs_error), to_host<T>(rhs)));
+  } else {
+    EXPECT_EQ(lhs, rhs);
+  }
+}
+
+#define CUSPATIAL_EXPECT_VECTORS_EQUIVALENT(lhs, rhs, ...) \
+  do {                                                     \
+    SCOPED_TRACE(" <--  line of failure\n");               \
+    expect_vector_equivalent(lhs, rhs, ##__VA_ARGS__);     \
   } while (0)
 
 }  // namespace test
