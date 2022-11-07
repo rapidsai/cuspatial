2e52af347f9a791933ec2d63edf75700e75f2387
Mark Harris <mharris@nvidia.com>
2022-11-08 07:53:36 +1100

    Document test utilities

--- cpp/include/cuspatial_test/test_util.cuh
+++ cpp/include/cuspatial_test/test_util.cuh
@@ -32,6 +32,17 @@ namespace cuspatial {
 
 namespace test {
 
+/**
+ * @brief Copy a device vector container to a thrust::host_vector
+ *
+ * Supports rmm::device_uvector as well as any host vector type from which a thrust::host_vector
+ * can be constructed.
+ *
+ * @tparam T The type of elements in the container
+ * @tparam Vector The device vector type
+ * @param dvec The device vector container to copy
+ * @return A thrust::host_vector containing a copy of the input device vector
+ */
 template <typename T, typename Vector>
 thrust::host_vector<T> to_host(Vector const& dvec)
 {
@@ -49,22 +60,41 @@ thrust::host_vector<T> to_host(Vector const& dvec)
   }
 }
 
+/**
+ * @brief Copy a device data range to a thrust::host_vector
+ *
+ * @tparam Iter Iterator to device data
+ * @tparam T
+ * @param begin The beginning of the range to copy
+ * @param end The end of the range to copy
+ * @return A thrust::host_vector containing a copy of the input device range
+ */
 template <typename Iter, typename T = cuspatial::iterator_value_type<Iter>>
 thrust::host_vector<T> to_host(Iter begin, Iter end)
 {
   return thrust::host_vector<T>(begin, end);
 }
 
+/**
+ * @brief Print a range of device-side data.
+ *
+ * @note Copies the data range to host before printing.
+ *
+ * @tparam Iter Iterator to device data
+ * @param begin The beginning of the range to print
+ * @param end The end of the range to print
+ * @param pre String to print before the device data range
+ * @param post String to print after the device data range
+ */
 template <typename Iter>
 void print_device_range(Iter begin,
                         Iter end,
                         std::string_view pre  = "",
-                        std::string_view post = "\n",
-                        int precision         = std::cout.precision())
+                        std::string_view post = "\n")
 {
   auto hvec = to_host(begin, end);
 
-  std::cout << std::setprecision(precision) << pre;
+  std::cout << pre;
   std::for_each(hvec.begin(), hvec.end(), [](auto const& x) { std::cout << x << " "; });
   std::cout << post;
 }
