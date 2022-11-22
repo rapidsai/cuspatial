1d158bb96c6f1a6979057bb4e5f9f0bfa3beaa94
Michael Wang <isVoid@users.noreply.github.com>
2022-11-22 10:41:26 -0800

    switch to order of template arguments

--- cpp/include/cuspatial/experimental/detail/linestring_intersection_count.cuh
+++ cpp/include/cuspatial/experimental/detail/linestring_intersection_count.cuh
@@ -16,7 +16,6 @@
 
 #pragma once
 
-#include <atomic>
 #include <cuspatial/cuda_utils.hpp>
 #include <cuspatial/detail/utility/device_atomics.cuh>
 #include <cuspatial/detail/utility/linestring.cuh>
@@ -50,11 +49,10 @@ __global__ void count_intersection_and_overlaps_simple(MultiLinestringRange1 mul
       for (auto [c, d] : linestring2) {
         auto [point_opt, segment_opt] = segment_intersection(segment<T>{a, b}, segment<T>{c, d});
         if (point_opt.has_value()) {
-          auto r = make_atomic_ref<T, cuda::thread_scope_device>(*(point_count_it + geometry_idx));
+          auto r = make_atomic_ref<cuda::thread_scope_device>(point_count_it[geometry_idx]);
           r.fetch_add(1, cuda::memory_order_relaxed);
         } else if (segment_opt.has_value()) {
-          auto r =
-            make_atomic_ref<T, cuda::thread_scope_device>(*(segment_count_it + geometry_idx));
+          auto r = make_atomic_ref<cuda::thread_scope_device>(segment_count_it[geometry_idx]);
           r.fetch_add(1, cuda::memory_order_relaxed);
         }
       }
