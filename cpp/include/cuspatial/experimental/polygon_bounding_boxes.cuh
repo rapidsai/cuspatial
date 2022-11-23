a23f06ab4fc5f665e5f1a3588a3a6a3e85e0b571
Mark Harris <mharris@nvidia.com>
2022-11-23 18:58:34 +1100

    Add failing tests and empty header-only implementation

---
+++ cpp/include/cuspatial/experimental/polygon_bounding_boxes.cuh
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2022, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <rmm/cuda_stream_view.hpp>
+
+namespace cuspatial {
+
+template <class OffsetIteratorA,
+          class OffsetIteratorB,
+          class PointIterator,
+          class BoundingBoxIterator>
+BoundingBoxIterator polygon_bounding_boxes(OffsetIteratorA polygon_offsets_first,
+                                           OffsetIteratorA polygon_offsets_last,
+                                           OffsetIteratorB polygon_ring_offsets_first,
+                                           OffsetIteratorB polygon_ring_offsets_last,
+                                           PointIterator polygon_points_first,
+                                           PointIterator polygon_points_last,
+                                           BoundingBoxIterator output_first,
+                                           rmm::cuda_stream_view stream = rmm::cuda_stream_default);
+}
+
+#include <cuspatial/experimental/detail/polygon_bounding_boxes.cuh>
