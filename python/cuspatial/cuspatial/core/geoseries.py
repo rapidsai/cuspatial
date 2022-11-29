ad308fb2e47fa53eb7218a4241109131d3e11d8b
H. Thomson Comer <thomcom@gmail.com>
2022-11-29 17:07:09 -0600

    Update python/cuspatial/cuspatial/core/geoseries.py
    Co-authored-by: Mark Harris <mharris@nvidia.com>

--- python/cuspatial/cuspatial/core/geoseries.py
+++ python/cuspatial/cuspatial/core/geoseries.py
@@ -645,7 +645,7 @@ class GeoSeries(cudf.Series):
 
     def contains(self, other, align=True):
         """Compute from a set of points and a set of polygons which points fall
-        within each polygon. Note that `polygons_(x,y)` must be specified as
+        strictly inside each polygon. Points on polygon edges and vertices are not contained in the polygon. Note that `polygons_(x,y)` must be specified as
         closed polygons: the first and last coordinate of each polygon must be
         the same.
 
