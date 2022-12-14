3e4bcc1c7b1243fca7dee9aceb93f8dd60a60783
Michael Wang <isVoid@users.noreply.github.com>
2022-12-14 11:13:34 -0800

    Update cpp/tests/experimental/spatial/linestring_intersection_intermediates_remove_if_test.cu
    Co-authored-by: H. Thomson Comer <thomcom@gmail.com>

--- cpp/tests/experimental/spatial/linestring_intersection_intermediates_remove_if_test.cu
+++ cpp/tests/experimental/spatial/linestring_intersection_intermediates_remove_if_test.cu
@@ -89,7 +89,7 @@ struct LinestringIntersectionIntermediatesRemoveIfTest : public ::testing::Test 
   }
 };
 
-// float and double are logically the same but would require seperate tests due to precision.
+// float and double are logically the same but would require separate tests due to precision.
 using TestTypes = ::testing::Types<float, double>;
 TYPED_TEST_CASE(LinestringIntersectionIntermediatesRemoveIfTest, TestTypes);
 
