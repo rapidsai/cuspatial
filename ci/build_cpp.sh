fc8fcb35853fd1b0c63f60e91551d20d8bff5d90
galipremsagar <sagarprem75@gmail.com>
2023-01-24 13:32:06 -0800

    temporary changes

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -7,10 +7,14 @@ source rapids-env-update
 
 export CMAKE_GENERATOR=Ninja
 
+LIBCUDF_CHANNEL=$(rapids-get-artifact ci/cudf/pull-request/12587/046025a/cudf_conda_cpp_cuda11_$(arch).tar.gz)
+
 rapids-print-env
 
 rapids-logger "Begin cpp build"
 
-rapids-mamba-retry mambabuild conda/recipes/libcuspatial
+rapids-mamba-retry mambabuild \
+    --channel "${LIBCUDF_CHANNEL}" \
+    conda/recipes/libcuspatial
 
 rapids-upload-conda-to-s3 cpp
