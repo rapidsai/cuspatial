9cb56ba22b354fddc19beea47b5034b7b99a01d7
Michael Wang <isVoid@users.noreply.github.com>
2023-02-09 21:16:36 -0800

    initial

--- python/cuspatial/cuspatial/_lib/cpp/distance/hausdorff.pxd
+++ python/cuspatial/cuspatial/_lib/cpp/distance/hausdorff.pxd
@@ -1,15 +1,17 @@
 # Copyright (c) 2020, NVIDIA CORPORATION.
 
 from libcpp.memory cimport unique_ptr
+from libcpp.utility cimport pair
 
 from cudf._lib.cpp.column.column cimport column
 from cudf._lib.cpp.column.column_view cimport column_view
+from cudf._lib.cpp.table.table_view cimport table_view
 
 
 cdef extern from "cuspatial/distance/hausdorff.hpp" \
         namespace "cuspatial" nogil:
 
-    cdef unique_ptr[column] directed_hausdorff_distance(
+    cdef pair[unique_ptr[column], table_view] directed_hausdorff_distance(
         const column_view& xs,
         const column_view& ys,
         const column_view& space_offsets
--- python/cuspatial/cuspatial/_lib/hausdorff.pyx
+++ python/cuspatial/cuspatial/_lib/hausdorff.pyx
@@ -1,9 +1,11 @@
 # Copyright (c) 2019, NVIDIA CORPORATION.
 
 from libcpp.memory cimport unique_ptr
-from libcpp.utility cimport move
+from libcpp.utility cimport move, pair
 
 from cudf._lib.column cimport Column, column, column_view
+from cudf._lib.cpp.table.table_view cimport table_view
+from cudf._lib.utils cimport columns_from_table_view
 
 from cuspatial._lib.cpp.distance.hausdorff cimport (
     directed_hausdorff_distance as directed_cpp_hausdorff_distance,
@@ -19,7 +21,7 @@ def directed_hausdorff_distance(
     cdef column_view c_ys = ys.view()
     cdef column_view c_shape_offsets = space_offsets.view()
 
-    cdef unique_ptr[column] result
+    cdef pair[unique_ptr[column], table_view] result
 
     with nogil:
         result = move(
@@ -30,4 +32,9 @@ def directed_hausdorff_distance(
             )
         )
 
-    return Column.from_unique_ptr(move(result))
+    owner = Column.from_unique_ptr(move(result.first), data_ptr_exposed=True)
+
+    return columns_from_table_view(
+        result.second,
+        owners=[owner] * result.second.num_columns()
+    )
