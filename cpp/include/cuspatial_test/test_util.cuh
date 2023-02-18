3228d1638dad98cd67affda90326dafa4395a110
Michael Wang <isVoid@users.noreply.github.com>
2023-02-17 17:13:24 -0800

    cleanup

--- cpp/include/cuspatial_test/test_util.cuh
+++ cpp/include/cuspatial_test/test_util.cuh
@@ -27,7 +27,6 @@
 #include <thrust/host_vector.h>
 
 #include <cstdio>
-#include <type_traits>
 
 namespace cuspatial {
 
@@ -99,37 +98,6 @@ void print_device_range(Iter begin,
   std::for_each(hvec.begin(), hvec.end(), [](auto const& x) { std::cout << x << " "; });
   std::cout << post;
 }
-
-
-/**
- * @brief Print a range of device-side data.
- *
- * @note Copies the data range to host before printing.
- *
- * @tparam Iter Iterator to device data
- * @param begin The beginning of the range to print
- * @param end The end of the range to print
- * @param pre String to print before the device data range
- * @param post String to print after the device data range
- */
-template <typename Vec>
-void print_device_vector(Vec const& v,
-                        std::string_view pre  = "",
-                        std::string_view post = "\n")
-{
-  using T = typename Vec::value_type;
-  auto hvec = to_host<T>(v);
-
-  std::cout << pre;
-  if constexpr(std::is_integral<T>())
-    std::for_each(hvec.begin(), hvec.end(), [](auto const& x) { std::cout << static_cast<int>(x) << " "; });
-  else
-    std::for_each(hvec.begin(), hvec.end(), [](auto const& x) { std::cout << x << " "; });
-  std::cout << post;
-}
-
-
-
 
 }  // namespace test
 }  // namespace cuspatial
