fba54987ed5414d00e7044287f9b5eafecd88699
ptaylor <paul.e.taylor@me.com>
2023-02-22 07:04:57 +0000

    use rapids-build-utils feature

--- .devcontainer/opt/rapids/bin/update-content-command.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-mkdir -m 0755 -p ~/{.aws,.cache,.conda,.config,rmm,cudf};
-
-cat <<"EOF" >> ~/.bashrc
-if [[ "$PATH" != *"/opt/rapids/bin"* ]]; then
-    export PATH="$PATH:/opt/rapids/bin";
-fi
-EOF
