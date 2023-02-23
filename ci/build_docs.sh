9a962a5525729580345564971609032f0af90459
Jake Awe <jawe@nvidia.com>
2023-02-23 15:45:00 -0600

    add logger stmt

--- ci/build_docs.sh
+++ ci/build_docs.sh
@@ -40,6 +40,7 @@ popd
 
 
 if [[ ${RAPIDS_BUILD_TYPE} == "branch" ]]; then
+  rapids-logger "Upload Docs to S3"
   aws s3 sync --delete docs/_html "s3://rapidsai-docs/cuspatial/${VERSION_NUMBER}/html"
   aws s3 sync --delete docs/_text "s3://rapidsai-docs/cuspatial/${VERSION_NUMBER}/txt"
   aws s3 sync --delete cpp/doxygen/html "s3://rapidsai-docs/libcuspatial/${VERSION_NUMBER}/html"
