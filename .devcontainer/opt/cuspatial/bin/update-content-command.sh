0d856283dc3e6ceca9b3884a304225e569c130f1
ptaylor <paul.e.taylor@me.com>
2023-02-28 14:26:52 -0800

    use LLVM 16

--- .devcontainer/opt/cuspatial/bin/update-content-command.sh
+++ .devcontainer/opt/cuspatial/bin/update-content-command.sh
@@ -1,3 +1,7 @@
 #! /usr/bin/env bash
 
+mkdir -m 0755 -p ~/.{aws,cache,config/clangd,conda,local};
+
+cp /opt/cuspatial/.clangd ~/.config/clangd/config.yaml;
+
 rapids-make-vscode-workspace --update;
