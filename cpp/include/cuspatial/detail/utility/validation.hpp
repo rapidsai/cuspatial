8f72d82a438ceaea21e8cd752f37bef37d461a48
Michael Wang <isVoid@users.noreply.github.com>
2023-03-21 15:40:45 -0700

    Add column API for `pairwise_point_polygon_distance` (#984)
    This PR adds column API for `pairwise_point_polygon_distance`.
    
    Authors:
      - Michael Wang (https://github.com/isVoid)
    
    Approvers:
      - Mark Harris (https://github.com/harrism)
      - H. Thomson Comer (https://github.com/thomcom)
    
    URL: https://github.com/rapidsai/cuspatial/pull/984

--- cpp/include/cuspatial/detail/utility/validation.hpp
+++ cpp/include/cuspatial/detail/utility/validation.hpp
@@ -48,3 +48,31 @@
                     "Each polygon must have at least one (1) ring");                              \
   CUSPATIAL_EXPECTS(num_poly_points >= 4 * (num_poly_ring_offsets - 1),                           \
                     "Each ring must have at least four (4) vertices");
+
+/**
+ * @brief Macro for validating the data array sizes for a multipolygon.
+ *
+ * Raises an exception if any of the following are false:
+ *  - The number of multipolygon offsets is greater than zero.
+ *  - The number of polygon offsets is greater than zero.
+ *  - The number of ring offsets is greater than zero.
+ *  - There is at least one ring offset per polygon offset.
+ *  - There are at least four vertices per ring offset.
+ *
+ * MultiPolygons follow [GeoArrow data layout][1]. Offsets arrays (polygons and rings) have one more
+ * element than the number of items in the array. The last offset is always the sum of the previous
+ * offset and the size of that element. For example the last value in the ring offsets array is the
+ * last ring offset plus the number of rings in the last polygon. See
+ * [Arrow Variable-Size Binary layout](2). Note that an empty list still has one offset: {0}.
+ *
+ * Rings are assumed to be closed (closed means the first and last vertices of
+ * each ring are equal). Therefore rings must have at least 4 vertices.
+ *
+ * [1]: https://github.com/geoarrow/geoarrow/blob/main/format.md
+ * [2]: https://arrow.apache.org/docs/format/Columnar.html#variable-size-binary-layout
+ */
+#define CUSPATIAL_EXPECTS_VALID_MULTIPOLYGON_SIZES(                                \
+  num_poly_points, num_multipoly_offsets, num_poly_offsets, num_poly_ring_offsets) \
+  CUSPATIAL_EXPECTS(num_multipoly_offsets > 0,                                     \
+                    "Multipolygon offsets must contain at least one (1) value");   \
+  CUSPATIAL_EXPECTS_VALID_POLYGON_SIZES(num_poly_points, num_poly_offsets, num_poly_ring_offsets);
