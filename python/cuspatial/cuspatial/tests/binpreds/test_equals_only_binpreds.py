3b6d9313286cfa39ad61282c47ba0bf10d2ccf02
H. Thomson Comer <thomcom@gmail.com>
2023-03-27 19:54:59 +0000

    Remove old binpreds.

--- python/cuspatial/cuspatial/tests/binpreds/test_equals_only_binpreds.py
+++ python/cuspatial/cuspatial/tests/binpreds/test_equals_only_binpreds.py
@@ -732,50 +732,6 @@ def test_linestring_orders():
     pd.testing.assert_series_equal(expected, got.to_pandas())
 
 
-def test_internal_reversed_linestrings():
-    linestring1 = cuspatial.GeoSeries(
-        [
-            LineString([(0, 0), (1, 1), (1, 0), (0, 0)]),
-        ]
-    )
-    linestring2 = cuspatial.GeoSeries(
-        [
-            LineString([(0, 0), (1, 0), (1, 1), (0, 0)]),
-        ]
-    )
-    from cuspatial.core.binpreds.binpreds import EqualsBinpred
-
-    bp = EqualsBinpred(linestring1, linestring2)
-    got = bp._reverse_linestrings(
-        linestring1.lines.xy, linestring1.lines.part_offset
-    ).to_pandas()
-    expected = linestring2.lines.xy.to_pandas()
-    pd.testing.assert_series_equal(got, expected)
-
-
-def test_internal_reversed_linestrings_pair():
-    linestring1 = cuspatial.GeoSeries(
-        [
-            LineString([(0, 0), (1, 1), (1, 0), (0, 0)]),
-            LineString([(0, 0), (1, 1), (1, 0)]),
-        ]
-    )
-    linestring2 = cuspatial.GeoSeries(
-        [
-            LineString([(0, 0), (1, 0), (1, 1), (0, 0)]),
-            LineString([(1, 0), (1, 1), (0, 0)]),
-        ]
-    )
-    from cuspatial.core.binpreds.binpreds import EqualsBinpred
-
-    bp = EqualsBinpred(linestring1, linestring2)
-    got = bp._reverse_linestrings(
-        linestring1.lines.xy, linestring1.lines.part_offset
-    ).to_pandas()
-    expected = linestring2.lines.xy.to_pandas()
-    pd.testing.assert_series_equal(got, expected)
-
-
 def test_from_points_xy_large():
     points = cuspatial.GeoSeries(
         cuspatial.core._column.geocolumn.GeoColumn._from_points_xy(
