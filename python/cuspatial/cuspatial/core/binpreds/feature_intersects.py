49ddc9d0e338d4c15de528e1c32ac636aba0f8a0
H. Thomson Comer <thomcom@gmail.com>
2023-03-27 16:47:09 -0500

    Update Intersects

--- python/cuspatial/cuspatial/core/binpreds/feature_intersects.py
+++ python/cuspatial/cuspatial/core/binpreds/feature_intersects.py
@@ -1,15 +1,25 @@
 # Copyright (c) 2023, NVIDIA CORPORATION.
 
-from cuspatial.core._column.geocolumn import ColumnType
 from cuspatial.core.binpreds.binpred_interface import NotImplementedRoot
 from cuspatial.core.binpreds.feature_contains import RootContains
 from cuspatial.core.binpreds.feature_equals import RootEquals
+from cuspatial.utils.binpred_utils import (
+    LineString,
+    MultiPoint,
+    Point,
+    Polygon,
+)
 
 
 class RootIntersects(RootEquals):
-    """Base class for binary predicates that are defined in terms of a
-    root-level binary predicate. For example, a Point-Point Intersects
-    predicate is defined in terms of a Point-Point Intersects predicate.
+    """Base class for binary predicates that are defined in terms of
+    the intersects basic predicate. These predicates are defined in terms
+    of the equals basic predicate. The type dispatches here that depend
+    on `RootIntersects` use the `RootEquals` class for their complete
+    implementation, unmodified.
+
+    point.intersects(polygon) is equivalent to polygon.contains(point)
+    with the left and right hand sides reversed.
     """
 
     pass
@@ -17,16 +27,13 @@ class RootIntersects(RootEquals):
 
 class PointPolygonIntersects(RootContains):
     def _preprocess(self, lhs, rhs):
+        """Swap LHS and RHS and call the normal contains processing."""
         self.lhs = rhs
         self.rhs = lhs
         return super()._preprocess(rhs, lhs)
 
 
-Point = ColumnType.POINT
-MultiPoint = ColumnType.MULTIPOINT
-LineString = ColumnType.LINESTRING
-Polygon = ColumnType.POLYGON
-
+""" Type dispatch dictionary for intersects binary predicates. """
 DispatchDict = {
     (Point, Point): RootIntersects,
     (Point, MultiPoint): NotImplementedRoot,
