195a275f17c6f6e2994fba8466eadbef44eaade7
Mark Harris <mharris@nvidia.com>
2023-04-04 03:45:50 +0000

    copyright

--- cpp/tests/indexing/point_quadtree_test.cpp
+++ cpp/tests/indexing/point_quadtree_test.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, NVIDIA CORPORATION.
+ * Copyright (c) 2020-2023, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,8 +27,7 @@
 #include <cudf_test/table_utilities.hpp>
 #include <cudf_test/type_lists.hpp>
 
-struct QuadtreeOnPointErrorTest : public cudf::test::BaseFixture {
-};
+struct QuadtreeOnPointErrorTest : public cudf::test::BaseFixture {};
 
 TYPED_TEST_CASE(QuadtreeOnPointIndexingTest, cudf::test::FloatingPointTypes);
 
