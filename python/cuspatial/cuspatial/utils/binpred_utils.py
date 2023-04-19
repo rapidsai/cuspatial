f45acda8b594a7e2fd27f707ae4fbfc1c2ab487a
H. Thomson Comer <thomcom@gmail.com>
2023-04-19 22:42:47 +0000

    Document and improve binpred_utils.py

--- python/cuspatial/cuspatial/utils/binpred_utils.py
+++ python/cuspatial/cuspatial/utils/binpred_utils.py
@@ -58,6 +58,8 @@ def _count_results_in_multipoint_geometries(point_indices, point_result):
 
 
 def _linestrings_from_polygons(geoseries):
+    """Converts the exterior and interior rings of a geoseries of polygons
+    into a geoseries of linestrings."""
     xy = geoseries.polygons.xy
     parts = geoseries.polygons.part_offset.take(
         geoseries.polygons.geometry_offset
@@ -71,7 +73,8 @@ def _linestrings_from_polygons(geoseries):
 
 
 def _linestrings_from_multipoints(geoseries):
-    """Convert rhs to linestrings."""
+    """Converts a geoseries of multipoints into a geoseries of
+    linestrings."""
     points = cudf.DataFrame(
         {
             "x": geoseries.multipoints.x.repeat(2).reset_index(drop=True),
@@ -87,8 +90,27 @@ def _linestrings_from_multipoints(geoseries):
 
 
 def _linestrings_from_points(geoseries):
-    """Convert rhs to linestrings.
-    TODO: Document"""
+    """Converts a geoseries of points into a geoseries of linestrings.
+
+    Linestrings converted to points are represented as a segment of
+    length two, with the beginning and ending of the segment being the
+    same point.
+
+    Example
+    -------
+    >>> import cuspatial
+    >>> from cuspatial.utils.binpred_utils import (
+    ...     _linestrings_from_points
+    ... )
+    >>> from shapely.geometry import Point
+    >>> points = cuspatial.GeoSeries([Point(0, 0), Point(1, 1)])
+    >>> linestrings = _linestrings_from_points(points)
+    >>> linestrings
+    Out[1]:
+    0    LINESTRING (0.00000 0.00000, 0.00000 0.00000)
+    1    LINESTRING (1.00000 1.00000, 1.00000 1.00000)
+    dtype: geometry
+    """
     x = cp.repeat(geoseries.points.x, 2)
     y = cp.repeat(geoseries.points.y, 2)
     xy = cudf.DataFrame({"x": x, "y": y}).interleave_columns()
@@ -98,7 +120,8 @@ def _linestrings_from_points(geoseries):
 
 
 def _linestrings_from_geometry(geoseries):
-    """Convert rhs to linestrings."""
+    """Wrapper function that converts any homogeneous geoseries into
+    a geoseries of linestrings."""
     if geoseries.column_type == ColumnType.POINT:
         return _linestrings_from_points(geoseries)
     if geoseries.column_type == ColumnType.MULTIPOINT:
@@ -114,7 +137,8 @@ def _linestrings_from_geometry(geoseries):
 
 
 def _multipoints_from_points(geoseries):
-    """Convert rhs to multipoints."""
+    """Converts a geoseries of points into a geoseries of length 1
+    multipoints."""
     result = cuspatial.GeoSeries.from_multipoints_xy(
         geoseries.points.xy, cp.arange((len(geoseries) + 1))
     )
@@ -122,15 +146,17 @@ def _multipoints_from_points(geoseries):
 
 
 def _multipoints_from_linestrings(geoseries):
-    """Convert rhs to multipoints."""
+    """Converts a geoseries of linestrings into a geoseries of
+    multipoints. MultiLineStrings are converted into a single multipoint."""
     xy = geoseries.lines.xy
     mpoints = geoseries.lines.part_offset.take(geoseries.lines.geometry_offset)
     return cuspatial.GeoSeries.from_multipoints_xy(xy, mpoints)
 
 
 def _multipoints_from_polygons(geoseries):
-    """Convert rhs to multipoints.
-    TODO: Document"""
+    """Converts a geoseries of polygons into a geoseries of multipoints.
+    All exterior and interior points become points in each multipoint object.
+    """
     xy = geoseries.polygons.xy
     polygon_offsets = geoseries.polygons.ring_offset.take(
         geoseries.polygons.part_offset.take(geoseries.polygons.geometry_offset)
@@ -139,7 +165,8 @@ def _multipoints_from_polygons(geoseries):
 
 
 def _multipoints_from_geometry(geoseries):
-    """Convert rhs to multipoints."""
+    """Wrapper function that converts any homogeneous geoseries into
+    a geoseries of multipoints."""
     if geoseries.column_type == ColumnType.POINT:
         return _multipoints_from_points(geoseries)
     elif geoseries.column_type == ColumnType.MULTIPOINT:
@@ -155,17 +182,22 @@ def _multipoints_from_geometry(geoseries):
 
 
 def _points_from_linestrings(geoseries):
-    """Convert rhs to points."""
+    """Convert a geoseries of linestrings into a geoseries of points.
+    The length of the result is equal to the sum of the lengths of the
+    linestrings in the original geoseries."""
     return cuspatial.GeoSeries.from_points_xy(geoseries.lines.xy)
 
 
 def _points_from_polygons(geoseries):
-    """Convert rhs to points."""
+    """Convert a geoseries of linestrings into a geoseries of points.
+    The length of the result is equal to the sum of the lengths of the
+    polygons in the original geoseries."""
     return cuspatial.GeoSeries.from_points_xy(geoseries.polygons.xy)
 
 
 def _points_from_geometry(geoseries):
-    """Convert rhs to points."""
+    """Wrapper function that converts any homogeneous geoseries into
+    a geoseries of points."""
     if geoseries.column_type == ColumnType.POINT:
         return geoseries
     elif geoseries.column_type == ColumnType.LINESTRING:
@@ -179,15 +211,34 @@ def _points_from_geometry(geoseries):
 
 
 def _linestring_to_boundary(geoseries):
-    """Convert a linestrings column to a multipoints column
+    """Convert a geoseries of linestrings to a geoseries of multipoints
     containing only the start and end of the linestrings."""
-    xy = geoseries.lines.xy
-    mpoints = geoseries.lines.part_offset.take(geoseries.lines.geometry_offset)
+    x = geoseries.lines.x
+    y = geoseries.lines.y
+    starts = geoseries.lines.part_offset.take(geoseries.lines.geometry_offset)
+    ends = (starts - 1)[1:]
+    starts = starts[:-1]
+    points_x = cudf.DataFrame(
+        {
+            "starts": x[starts].reset_index(drop=True),
+            "ends": x[ends].reset_index(drop=True),
+        }
+    ).interleave_columns()
+    points_y = cudf.DataFrame(
+        {
+            "starts": y[starts].reset_index(drop=True),
+            "ends": y[ends].reset_index(drop=True),
+        }
+    ).interleave_columns()
+    xy = cudf.DataFrame({"x": points_x, "y": points_y}).interleave_columns()
+    mpoints = cp.arange(len(starts) + 1) * 2
     return cuspatial.GeoSeries.from_multipoints_xy(xy, mpoints)
 
 
 def _polygon_to_boundary(geoseries):
-    """Convert a polygon column to a linestring column."""
+    """Convert a geoseries of polygons to a geoseries of linestrings or
+    multilinestrings containing only the exterior and interior boundaries
+    of the polygons."""
     xy = geoseries.polygons.xy
     parts = geoseries.polygons.part_offset.take(
         geoseries.polygons.geometry_offset
@@ -201,7 +252,8 @@ def _polygon_to_boundary(geoseries):
 
 
 def _is_complex(geoseries):
-    """Returns True if the GeoSeries contains complex types."""
+    """Returns True if the GeoSeries contains non-point features that
+    need to be reconstructed after basic predicates have computed."""
     if len(geoseries.polygons.xy) > 0:
         return True
     if len(geoseries.lines.xy) > 0:
