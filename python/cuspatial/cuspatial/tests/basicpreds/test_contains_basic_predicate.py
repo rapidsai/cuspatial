d7fffac34216530d4f371c4c2bcea73653d4c77a
H. Thomson Comer <thomcom@gmail.com>
2023-04-27 21:34:21 +0000

    Remove unsupported test.

--- python/cuspatial/cuspatial/tests/basicpreds/test_contains_basic_predicate.py
+++ python/cuspatial/cuspatial/tests/basicpreds/test_contains_basic_predicate.py
@@ -5,38 +5,6 @@ from shapely.geometry import LineString, Point, Polygon
 import cuspatial
 
 
-def test_basic_contains_none_outside():
-    lhs = cuspatial.GeoSeries([Polygon([(0, 0), (0, 1), (1, 1), (1, 0)])])
-    rhs = cuspatial.GeoSeries([Point(2, 2)])
-    got = lhs._basic_contains_none(rhs).to_pandas()
-    expected = [True]
-    assert (got == expected).all()
-
-
-def test_basic_contains_none_inside():
-    lhs = cuspatial.GeoSeries([Polygon([(0, 0), (0, 1), (1, 1), (1, 0)])])
-    rhs = cuspatial.GeoSeries([Point(0.5, 0.5)])
-    got = lhs._basic_contains_none(rhs).to_pandas()
-    expected = [False]
-    assert (got == expected).all()
-
-
-def test_basic_contains_none_point():
-    lhs = cuspatial.GeoSeries([Polygon([(0, 0), (0, 1), (1, 1), (1, 0)])])
-    rhs = cuspatial.GeoSeries([Point(0, 0)])
-    got = lhs._basic_contains_none(rhs).to_pandas()
-    expected = [False]
-    assert (got == expected).all()
-
-
-def test_basic_contains_none_edge():
-    lhs = cuspatial.GeoSeries([Polygon([(0, 0), (0, 1), (1, 1), (1, 0)])])
-    rhs = cuspatial.GeoSeries([Point(0, 0.5)])
-    got = lhs._basic_contains_none(rhs).to_pandas()
-    expected = [False]
-    assert (got == expected).all()
-
-
 def test_basic_contains_any_outside():
     lhs = cuspatial.GeoSeries([Polygon([(0, 0), (0, 1), (1, 1), (1, 0)])])
     rhs = cuspatial.GeoSeries([Point(2, 2)])
