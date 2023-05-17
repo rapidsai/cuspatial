fcbe105dabaf3bca3df064754ee66c9d5332deba
Michael Wang <isVoid@users.noreply.github.com>
2023-05-17 09:20:47 -0700

    remove incomplete validations and add implicit assumptions in documentation

--- cpp/include/cuspatial/detail/utility/validation.hpp
+++ cpp/include/cuspatial/detail/utility/validation.hpp
@@ -36,9 +36,7 @@
  */
 #define CUSPATIAL_EXPECTS_VALID_LINESTRING_SIZES(num_linestring_points, num_linestring_offsets) \
   CUSPATIAL_HOST_DEVICE_EXPECTS(num_linestring_offsets > 0,                                     \
-                                "Polygon offsets must contain at least one (1) value");         \
-  CUSPATIAL_HOST_DEVICE_EXPECTS(num_linestring_points >= 2 * (num_linestring_offsets - 1),      \
-                                "Each linestring must have at least two vertices");
+                                "Polygon offsets must contain at least one (1) value");
 
 /**
  * @brief Macro for validating the data array sizes for multilinestrings.
@@ -69,8 +67,6 @@
  * Raises an exception if any of the following are false:
  *  - The number of polygon offsets is greater than zero.
  *  - The number of ring offsets is greater than zero.
- *  - There is at least one ring offset per polygon offset.
- *  - There are at least four vertices per ring offset.
  *
  * Polygons follow [GeoArrow data layout][1]. Offsets arrays (polygons and rings) have one more
  * element than the number of items in the array. The last offset is always the sum of the previous
@@ -78,8 +74,11 @@
  * last ring offset plus the number of rings in the last polygon. See
  * [Arrow Variable-Size Binary layout](2). Note that an empty list still has one offset: {0}.
  *
- * Rings are assumed to be closed (closed means the first and last vertices of
- * each ring are equal). Therefore rings must have at least 4 vertices.
+ * The following are not explicitly checked in this macro, but is always assumed in cuspatial:
+ *
+ * 1. Polygon can contain zero (0) or more rings. A polygon with zero rings is an empty polygon.
+ * 2. Rings are assumed to be closed (closed means the first and last vertices of each ring are equal).
+ * Rings can also be empty. Therefore each ring must contain zero (0) or four (4) or more coordinates.
  *
  * [1]: https://github.com/geoarrow/geoarrow/blob/main/format.md
  * [2]: https://arrow.apache.org/docs/format/Columnar.html#variable-size-binary-layout
@@ -89,11 +88,7 @@
   CUSPATIAL_HOST_DEVICE_EXPECTS(num_poly_offsets > 0,                                        \
                                 "Polygon offsets must contain at least one (1) value");      \
   CUSPATIAL_HOST_DEVICE_EXPECTS(num_poly_ring_offsets > 0,                                   \
-                                "Polygon ring offsets must contain at least one (1) value"); \
-  CUSPATIAL_HOST_DEVICE_EXPECTS(num_poly_ring_offsets >= num_poly_offsets,                   \
-                                "Each polygon must have at least one (1) ring");             \
-  CUSPATIAL_HOST_DEVICE_EXPECTS(num_poly_points >= 4 * (num_poly_ring_offsets - 1),          \
-                                "Each ring must have at least four (4) vertices");
+                                "Polygon ring offsets must contain at least one (1) value");
 
 /**
  * @brief Macro for validating the data array sizes for a multipolygon.
@@ -102,8 +97,6 @@
  *  - The number of multipolygon offsets is greater than zero.
  *  - The number of polygon offsets is greater than zero.
  *  - The number of ring offsets is greater than zero.
- *  - There is at least one ring offset per polygon offset.
- *  - There are at least four vertices per ring offset.
  *
  * MultiPolygons follow [GeoArrow data layout][1]. Offsets arrays (polygons and rings) have one more
  * element than the number of items in the array. The last offset is always the sum of the previous
@@ -111,8 +104,11 @@
  * last ring offset plus the number of rings in the last polygon. See
  * [Arrow Variable-Size Binary layout](2). Note that an empty list still has one offset: {0}.
  *
- * Rings are assumed to be closed (closed means the first and last vertices of
- * each ring are equal). Therefore rings must have at least 4 vertices.
+ * The following are not explicitly checked in this macro, but is always assumed in cuspatial:
+ *
+ * 1. Polygon can contain zero (0) or more rings. A polygon with zero rings is an empty polygon.
+ * 2. Rings are assumed to be closed (closed means the first and last vertices of each ring are equal).
+ * Rings can also be empty. Therefore each ring must contain zero (0) or four (4) or more coordinates.
  *
  * [1]: https://github.com/geoarrow/geoarrow/blob/main/format.md
  * [2]: https://arrow.apache.org/docs/format/Columnar.html#variable-size-binary-layout
