9e1180a65dbf3c02a7e6b62084cf181e29a549c7
Michael Wang <isVoid@users.noreply.github.com>
2023-06-08 11:43:56 -0700

    update pairwise documentation

--- cpp/include/cuspatial/point_in_polygon.cuh
+++ cpp/include/cuspatial/point_in_polygon.cuh
@@ -114,49 +114,30 @@ OutputIt point_in_polygon(Cart2dItA test_points_first,
                           rmm::cuda_stream_view stream = rmm::cuda_stream_default);
 
 /**
- * @brief Given (point, polygon) pairs, tests whether the point of each pair is inside the polygon
- * of the pair.
+ * @brief Given (multipoint, multipolygon) pairs, tests whether any of the multipoint in the pair
+ * is in any of the multipolygon in the pair.
  *
- * Tests whether each point is inside a corresponding polygon.  Points on the edges of the
- * polygon are not considered to be inside.
- * Polygons are a collection of one or more rings. Rings are a collection of three or more vertices.
+ * For each of the point in the multipoint, test if the point is in any of the polygon in the multipolygon.
+ * Points on the edges of the polygon are not considered to be inside.
  *
  * Each input point will map to one `int32_t` element in the output.
  *
- *
- * @tparam Cart2dItA iterator type for point array. Must meet
- * the requirements of [LegacyRandomAccessIterator][LinkLRAI] and be device-accessible.
- * @tparam Cart2dItB iterator type for point array. Must meet
- * the requirements of [LegacyRandomAccessIterator][LinkLRAI] and be device-accessible.
- * @tparam OffsetIteratorA iterator type for offset array. Must meet
- * the requirements of [LegacyRandomAccessIterator][LinkLRAI] and be device-accessible.
- * @tparam OffsetIteratorB iterator type for offset array. Must meet
- * the requirements of [LegacyRandomAccessIterator][LinkLRAI] and be device-accessible.
+ * @tparam MultiPointRange an instance of template type `multipoint_range`
+ * @tparam MultiPolygonRange an instance of template type `multipolygon_range`
  * @tparam OutputIt iterator type for output array. Must meet
  * the requirements of [LegacyRandomAccessIterator][LinkLRAI], be device-accessible, mutable and
  * iterate on `int32_t` type.
  *
- * @param test_points_first begin of range of test points
- * @param test_points_last end of range of test points
- * @param polygon_offsets_first begin of range of indices to the first ring in each polygon
- * @param polygon_offsets_last end of range of indices to the first ring in each polygon
- * @param ring_offsets_first begin of range of indices to the first point in each ring
- * @param ring_offsets_last end of range of indices to the first point in each ring
- * @param polygon_points_first begin of range of polygon points
- * @param polygon_points_last end of range of polygon points
+ * @param multipoints Range of multipoints, one per computed point-in-polygon pair
+ * @param multipolygons Range of multipolygons, one per comptued point-in-polygon pair
  * @param output begin iterator to the output buffer
  * @param stream The CUDA stream to use for kernel launches.
  * @return iterator to one past the last element in the output buffer
  *
  * @note Direction of rings does not matter.
- * @note This algorithm supports the ESRI shapefile format, but assumes all polygons are "clean" (as
- * defined by the format), and does _not_ verify whether the input adheres to the shapefile format.
  * @note The points of the rings must be explicitly closed.
  * @note Overlapping rings negate each other. This behavior is not limited to a single negation,
  * allowing for "islands" within the same polygon.
- * @note `poly_ring_offsets` must contain only the rings that make up the polygons indexed by
- * `poly_offsets`. If there are rings in `poly_ring_offsets` that are not part of the polygons in
- * `poly_offsets`, results are likely to be incorrect and behavior is undefined.
  *
  * ```
  *   poly w/two rings         poly w/four rings
@@ -171,13 +152,7 @@ OutputIt point_in_polygon(Cart2dItA test_points_first,
  *        +-----------+   +------------------------+
  * ```
  *
- * @pre All point iterators must have the same `vec_2d` value type, with  the same underlying
- * floating-point coordinate type (e.g. `cuspatial::vec_2d<float>`).
- * @pre All offset iterators must have the same integral value type.
  * @pre Output iterator must be mutable and iterate on int32_t type.
- *
- * @throw cuspatial::logic_error polygon has less than 1 ring.
- * @throw cuspatial::logic_error polygon has less than 4 vertices.
  *
  * [LinkLRAI]: https://en.cppreference.com/w/cpp/named_req/RandomAccessIterator
  * "LegacyRandomAccessIterator"
