3400ab9cee83453df325a4ee0870d2d17851363a
Michael Wang <isVoid@users.noreply.github.com>
2023-06-13 14:59:31 -0700

    temporarily using 1 process to run

--- ci/test_python.sh
+++ ci/test_python.sh
@@ -49,7 +49,7 @@ pushd python/cuspatial/cuspatial
 pytest \
   --cache-clear \
   --junitxml="${RAPIDS_TESTS_DIR}/junit-cuspatial.xml" \
-  --numprocesses=8 \
+  --numprocesses=1 \
   --dist=loadscope \
   --cov-config=../.coveragerc \
   --cov=cuspatial \
