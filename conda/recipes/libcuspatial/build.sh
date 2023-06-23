2a47eed91e63921bb4548b77c2d5735a7dc89169
Michael Wang <isVoid@users.noreply.github.com>
2023-06-23 21:32:55 +0000

    disable cupti in nvbench

--- conda/recipes/libcuspatial/build.sh
+++ conda/recipes/libcuspatial/build.sh
@@ -1,4 +1,5 @@
 # Copyright (c) 2018-2023, NVIDIA CORPORATION.
 
 # build cuspatial with verbose output
-./build.sh -v libcuspatial tests benchmarks --allgpuarch -n
+./build.sh -v libcuspatial tests benchmarks --allgpuarch -n \
+    --cmake_args=\"-DNVBench_ENABLE_CUPTI=OFF\"
