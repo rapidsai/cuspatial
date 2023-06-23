1b948db89e1a67da69f0f5e39e47ca11ad031fa1
Michael Wang <isVoid@users.noreply.github.com>
2023-06-23 12:34:46 -0700

    update meta.yaml, dependencies according to cuda12 bring up instructions

--- conda/recipes/libcuspatial/build.sh
+++ conda/recipes/libcuspatial/build.sh
@@ -1,6 +1,4 @@
-# Copyright (c) 2018-2022, NVIDIA CORPORATION.
-
-export NVCC_PREPEND_FLAGS="${NVCC_PREPEND_FLAGS} -ccbin ${CXX}" # Needed for CUDA 12 nvidia channel compilers
+# Copyright (c) 2018-2023, NVIDIA CORPORATION.
 
 # build cuspatial with verbose output
 ./build.sh -v libcuspatial tests benchmarks --allgpuarch -n
