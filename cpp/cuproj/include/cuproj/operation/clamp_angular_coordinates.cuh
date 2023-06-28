ade7cdc55fdf1ca96d799f3769699afbab6cc2e8
Mark Harris <mharris@nvidia.com>
2023-06-28 13:01:41 +0000

    Document clamp_angular_coordinates

--- cpp/cuproj/include/cuproj/operation/clamp_angular_coordinates.cuh
+++ cpp/cuproj/include/cuproj/operation/clamp_angular_coordinates.cuh
@@ -27,6 +27,13 @@ namespace cuproj {
 
 static constexpr double M_TWOPI = 6.283185307179586476925286766559005;
 
+/**
+ * @brief Clamp longitude to the -pi:pi range
+ *
+ * @tparam T data type
+ * @param longitude The longitude to clamp
+ * @return The clamped longitude
+ */
 template <typename T>
 __host__ __device__ T clamp_longitude(T longitude)
 {
@@ -43,10 +50,22 @@ __host__ __device__ T clamp_longitude(T longitude)
   return longitude - M_PI;
 }
 
+/**
+ * @brief Clamp angular coordinates to the valid range
+ *
+ * @tparam Coordinate the coordinate type
+ * @tparam T the coordinate value type
+ */
 template <typename Coordinate, typename T = typename Coordinate::value_type>
-struct clamp_angular_coordinates : operation<Coordinate> {
-  static constexpr T EPS_LAT = 1e-12;
+class clamp_angular_coordinates : operation<Coordinate> {
+ public:
+  static constexpr T EPS_LAT = 1e-12;  // epsilon for latitude
 
+  /**
+   * @brief Construct a new clamp angular coordinates object
+   *
+   * @param params the projection parameters
+   */
   __host__ __device__ clamp_angular_coordinates(projection_parameters<T> const& params)
     : lam0_(params.lam0_), prime_meridian_offset_(params.prime_meridian_offset_)
   {
@@ -54,6 +73,13 @@ struct clamp_angular_coordinates : operation<Coordinate> {
 
   // projection_parameters<T> setup(projection_parameters<T> const& params) { return params; }
 
+  /**
+   * @brief Clamp angular coordinate to the valid range
+   *
+   * @param coord The coordinate to clamp
+   * @param dir The direction of the operation
+   * @return The clamped coordinate
+   */
   __host__ __device__ Coordinate operator()(Coordinate const& coord, direction dir) const
   {
     if (dir == direction::FORWARD)
@@ -63,6 +89,16 @@ struct clamp_angular_coordinates : operation<Coordinate> {
   }
 
  private:
+  /**
+   * @brief Forward clamping operation
+   *
+   * Offsets the longitude by the prime meridian offset and central meridian
+   * and clamps the latitude to the range -pi/2..pi/2 radians (-90..90 degrees)
+   * and the longitude to the range -pi..pi radians (-180..180 degrees)
+   *
+   * @param coord The coordinate to clamp
+   * @return The clamped coordinate
+   */
   __host__ __device__ Coordinate forward(Coordinate const& coord) const
   {
     // check for latitude or longitude over-range
@@ -90,6 +126,15 @@ struct clamp_angular_coordinates : operation<Coordinate> {
     return xy;
   }
 
+  /**
+   * @brief Inverse clamping operation
+   *
+   * Reverse-offsets the longitude by the prime meridian offset and central meridian
+   * and clamps the longitude to the range -pi..pi radians (-180..180 degrees)
+   *
+   * @param coord The coordinate to clamp
+   * @return The clamped coordinate
+   */
   __host__ __device__ Coordinate inverse(Coordinate const& coord) const
   {
     Coordinate xy = coord;
@@ -103,7 +148,7 @@ struct clamp_angular_coordinates : operation<Coordinate> {
     return xy;
   }
 
-  T lam0_{};
+  T lam0_{};  // central meridian
   T prime_meridian_offset_{};
 };
 
