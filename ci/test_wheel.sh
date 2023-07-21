1f8e173ecc5962a6278fc993c020c1a243022230
Vyas Ramasubramani <vyasr@nvidia.com>
2023-07-21 15:43:41 -0700

    Install gdal and fiona before cuspatial

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -7,11 +7,11 @@ mkdir -p ./dist
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 RAPIDS_PY_WHEEL_NAME="cuspatial_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./dist
 
-# echo to expand wildcard before adding `[extra]` requires for pip
-python -m pip install $(echo ./dist/cuspatial*.whl)[test]
-
 # Install additional dependencies
 apt update && DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends libgdal-dev && python -m pip install --no-binary fiona 'fiona>=1.8.19,<1.9'
+
+# echo to expand wildcard before adding `[extra]` requires for pip
+python -m pip install $(echo ./dist/cuspatial*.whl)[test]
 
 arch=$(uname -m)
 if [[ "${arch}" == "aarch64" && ${RAPIDS_BUILD_TYPE} == "pull-request" ]]; then
