133834aea3cffab7e8ee269e4306c652b51ec631
Vyas Ramasubramani <vyasr@nvidia.com>
2023-07-21 15:02:30 -0700

    Typo

--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -5,7 +5,7 @@ set -eoxu pipefail
 
 mkdir -p ./dist
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
-RAPIDS_PY_WHEEL_NAME="cudf_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./dist
+RAPIDS_PY_WHEEL_NAME="cuspatial_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./dist
 
 # echo to expand wildcard before adding `[extra]` requires for pip
 python -m pip install $(echo ./dist/cuspatial*.whl)[test]
