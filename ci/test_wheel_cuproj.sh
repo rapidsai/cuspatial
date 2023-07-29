4bd9e0d3167e28c2cddd1ba4fe8f3a403c22bfe8
Mark Harris <mharris@nvidia.com>
2023-07-29 22:14:39 +0000

    Try installing cuspatial in test_wheel_cuproj.sh

--- ci/test_wheel_cuproj.sh
+++ ci/test_wheel_cuproj.sh
@@ -12,6 +12,7 @@ apt update
 DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends libgdal-dev
 python -m pip install --no-binary fiona 'fiona>=1.8.19,<1.9'
 python -m pip install --no-binary cupy 'cupy>=12.0.0'
+python -m pip install --no-binary cuspatial 'cuspatial>=23.6.0'
 
 # echo to expand wildcard before adding `[extra]` requires for pip
 python -m pip install $(echo ./dist/cuproj*.whl)[test]
