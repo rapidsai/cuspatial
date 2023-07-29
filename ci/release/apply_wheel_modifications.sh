6ff898ddbe7fcb57a81deb574eb1191530568fb2
Mark Harris <mharris@nvidia.com>
2023-07-29 23:55:43 +0000

    Try using apply_wheel_modifications

--- ci/release/apply_wheel_modifications.sh
+++ ci/release/apply_wheel_modifications.sh
@@ -15,6 +15,7 @@ sed -i "s/^version = .*/version = \"${VERSION}\"/g" python/cuproj/pyproject.toml
 sed -i "s/^name = \"cuproj\"/name = \"cuproj${CUDA_SUFFIX}\"/g" python/cuproj/pyproject.toml
 sed -i "s/rmm==/rmm${CUDA_SUFFIX}==/g" python/cuproj/pyproject.toml
 sed -i "s/cudf==/cudf${CUDA_SUFFIX}==/g" python/cuproj/pyproject.toml
+sed -i "s/cuspatial==/cuspatial${CUDA_SUFFIX}==/g" python/cuproj/pyproject.toml
 
 if [[ $CUDA_SUFFIX == "-cu12" ]]; then
   sed -i "s/cupy-cuda11x/cupy-cuda12x/g" python/cuproj/pyproject.toml
