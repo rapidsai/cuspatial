ff386c0e08e51adba6df4ff8478c0ed6a9d53e08
H. Thomson Comer <thomcom@gmail.com>
2023-08-03 17:41:07 -0500

    Skip the two benchmarking notebooks.

--- ci/test_notebooks.sh
+++ ci/test_notebooks.sh
@@ -34,7 +34,7 @@ pushd notebooks
 
 # Add notebooks that should be skipped here
 # (space-separated list of filenames without paths)
-SKIPNBS=""
+SKIPNBS="binary_predicates.ipynb cuproj_benchmark.ipynb"
 
 EXITCODE=0
 trap "EXITCODE=1" ERR
