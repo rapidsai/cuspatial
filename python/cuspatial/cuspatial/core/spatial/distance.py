6275517e05e15ecbebe2f0cc864561b6711a5da9
Taurean Dyer <46935140+taureandyernv@users.noreply.github.com>
2023-08-14 13:10:48 -0700

    Update python/cuspatial/cuspatial/core/spatial/distance.py
    Co-authored-by: jakirkham <jakirkham@gmail.com>

--- python/cuspatial/cuspatial/core/spatial/distance.py
+++ python/cuspatial/cuspatial/core/spatial/distance.py
@@ -126,7 +126,6 @@ def haversine_distance(p1: GeoSeries, p2: GeoSeries):
     >>> # Calculate Haversine Distance of cuDF dataframe to comparator point
     >>> df['compare_dist'] = cuspatial.haversine_distance(cuGeoSeries, atlGeoSeries)
     >>> df.head()
-    
            latitude  longitude  compare_lat  compare_lng  compare_dist
     0       0.0        0.0          2.0          2.0    314.474805
     1       0.0        1.0          2.0          2.0    248.629315
