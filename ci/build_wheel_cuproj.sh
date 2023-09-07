bc3080194c4e723e9c83f3ac0365b0b66a300c01
divyegala <divyegala@gmail.com>
2023-09-07 11:52:16 -0700

    use alpha spec

--- ci/build_wheel_cuproj.sh
+++ ci/build_wheel_cuproj.sh
@@ -3,24 +3,4 @@
 
 set -euo pipefail
 
-source rapids-configure-sccache
-source rapids-date-string
-
-# Use gha-tools rapids-pip-wheel-version to generate wheel version then
-# update the necessary files
-version_override="$(rapids-pip-wheel-version ${RAPIDS_DATE_STRING})"
-
-RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
-
-ci/release/apply_wheel_modifications.sh ${version_override} "-${RAPIDS_PY_CUDA_SUFFIX}"
-echo "The package name and/or version was modified in the package source. The git diff is:"
-git diff
-
-cd python/cuproj
-
-SKBUILD_CONFIGURE_OPTIONS="-DCUPROJ_BUILD_WHEELS=ON" python -m pip wheel . -w dist -vvv --no-deps --disable-pip-version-check
-
-mkdir -p final_dist
-python -m auditwheel repair -w final_dist dist/*
-
-RAPIDS_PY_WHEEL_NAME="cuproj_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 final_dist
+ci/build_wheel.sh cuproj python/cuproj
