6c9ac1105a9e7473dad26431952fac77549b3c1a
H. Thomson Comer <thomcom@gmail.com>
2023-10-05 16:03:26 -0500

    Re-enable cuspatial tests and dependency. (#1253)
    A poorly understood cuproj->cuspatial dependency required that cuspatial be removed from cuproj's tests during the 23.08->23.10 merge rebase. This PR re-enables those tests.
    
    Authors:
      - H. Thomson Comer (https://github.com/thomcom)
      - Mark Harris (https://github.com/harrism)
      - Bradley Dice (https://github.com/bdice)
    
    Approvers:
      - Michael Wang (https://github.com/isVoid)
      - Mark Harris (https://github.com/harrism)
      - Bradley Dice (https://github.com/bdice)
      - AJ Schmidt (https://github.com/ajschmidt8)
    
    URL: https://github.com/rapidsai/cuspatial/pull/1253

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -34,9 +34,14 @@ if ! rapids-is-release-build; then
     alpha_spec=',>=0.0.0a0'
 fi
 
+# Add CUDA version suffix to dependencies
 sed -r -i "s/rmm(.*)\"/rmm${PACKAGE_CUDA_SUFFIX}\1${alpha_spec}\"/g" ${pyproject_file}
 if [[ ${package_name} == "cuspatial" ]]; then
     sed -r -i "s/cudf==(.*)\"/cudf${PACKAGE_CUDA_SUFFIX}==\1${alpha_spec}\"/g" ${pyproject_file}
+fi
+
+if [[ ${package_name} == "cuproj" ]]; then
+    sed -r -i "s/cuspatial==(.*)\"/cuspatial${PACKAGE_CUDA_SUFFIX}==\1${alpha_spec}\"/g" ${pyproject_file}
 fi
 
 if [[ $PACKAGE_CUDA_SUFFIX == "-cu12" ]]; then
