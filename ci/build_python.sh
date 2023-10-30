5799ccac5455a0c7025b4ce7137aed6b47b37096
Vyas Ramasubramani <vyasr@nvidia.com>
2023-10-30 10:28:01 -0700

    Remove now unnecessary quote stripping

--- ci/build_python.sh
+++ ci/build_python.sh
@@ -14,7 +14,7 @@ package_dir="python"
 version=$(rapids-generate-version)
 commit=$(git rev-parse HEAD)
 
-echo "${version}" | tr -d '"' > VERSION
+echo "${version}" > VERSION
 for package_name in cuspatial cuproj; do 
     sed -i "/^__git_commit__/ s/= .*/= \"${commit}\"/g" "${package_dir}/${package_name}/${package_name}/_version.py"
 done
