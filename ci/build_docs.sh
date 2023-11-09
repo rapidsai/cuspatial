71e83d962ff8a81eb8a01889bb2c5014a6c4149e
Ray Douglass <ray@raydouglass.com>
2023-11-09 16:32:48 -0500

    v24.02 Updates [skip ci]

--- ci/build_docs.sh
+++ ci/build_docs.sh
@@ -27,7 +27,7 @@ rapids-mamba-retry install \
   cuspatial \
   cuproj
 
-export RAPIDS_VERSION_NUMBER="23.12"
+export RAPIDS_VERSION_NUMBER="24.02"
 export RAPIDS_DOCS_DIR="$(mktemp -d)"
 
 rapids-logger "Build cuSpatial CPP docs"
