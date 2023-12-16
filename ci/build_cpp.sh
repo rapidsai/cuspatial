2cf23fb7c9deea1e26979442c20c3d25b580cfda
Bradley Dice <bdice@bradleydice.com>
2023-12-15 20:47:43 -0600

    Use upstream artifacts.

--- ci/build_cpp.sh
+++ ci/build_cpp.sh
@@ -9,11 +9,16 @@ export CMAKE_GENERATOR=Ninja
 
 rapids-print-env
 
+LIBRMM_CHANNEL=$(rapids-get-pr-conda-artifact rmm 1404 cpp)
+LIBCUDF_CHANNEL=$(rapids-get-pr-conda-artifact cudf 14576 cpp)
+
 version=$(rapids-generate-version)
 
 rapids-logger "Begin cpp build"
 
 RAPIDS_PACKAGE_VERSION=${version} rapids-conda-retry mambabuild \
+    --channel "${LIBRMM_CHANNEL}" \
+    --channel "${LIBCUDF_CHANNEL}" \
     conda/recipes/libcuspatial
 
 rapids-upload-conda-to-s3 cpp
--- ci/build_python.sh
+++ ci/build_python.sh
@@ -15,11 +15,15 @@ version=$(rapids-generate-version)
 commit=$(git rev-parse HEAD)
 
 echo "${version}" > VERSION
-for package_name in cuspatial cuproj; do 
+for package_name in cuspatial cuproj; do
     sed -i "/^__git_commit__/ s/= .*/= \"${commit}\"/g" "${package_dir}/${package_name}/${package_name}/_version.py"
 done
 
 CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
+LIBRMM_CHANNEL=$(rapids-get-pr-conda-artifact rmm 1404 cpp)
+RMM_CHANNEL=$(rapids-get-pr-conda-artifact rmm 1404 python)
+LIBCUDF_CHANNEL=$(rapids-get-pr-conda-artifact cudf 14576 cpp)
+CUDF_CHANNEL=$(rapids-get-pr-conda-artifact cudf 14576 python)
 
 rapids-logger "Begin py build cuSpatial"
 
@@ -28,6 +32,10 @@ rapids-logger "Begin py build cuSpatial"
 RAPIDS_PACKAGE_VERSION=${version} rapids-conda-retry mambabuild \
   --no-test \
   --channel "${CPP_CHANNEL}" \
+  --channel "${LIBRMM_CHANNEL}" \
+  --channel "${RMM_CHANNEL}" \
+  --channel "${LIBCUDF_CHANNEL}" \
+  --channel "${CUDF_CHANNEL}" \
   conda/recipes/cuspatial
 
 rapids-logger "Begin py build cuProj"
@@ -37,6 +45,10 @@ rapids-logger "Begin py build cuProj"
 RAPIDS_PACKAGE_VERSION=${version} rapids-conda-retry mambabuild \
   --no-test \
   --channel "${CPP_CHANNEL}" \
+  --channel "${LIBRMM_CHANNEL}" \
+  --channel "${RMM_CHANNEL}" \
+  --channel "${LIBCUDF_CHANNEL}" \
+  --channel "${CUDF_CHANNEL}" \
   conda/recipes/cuproj
 
 rapids-upload-conda-to-s3 python
