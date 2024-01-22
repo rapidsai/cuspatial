caa121886181c77830f1f580fa92c93432d2e146
Bradley Dice <bdice@bradleydice.com>
2024-01-22 11:04:10 -0600

    Remove -DNVBench_ENABLE_CUPTI=OFF.

--- conda/recipes/libcuspatial/build.sh
+++ conda/recipes/libcuspatial/build.sh
@@ -1,5 +1,4 @@
 # Copyright (c) 2018-2023, NVIDIA CORPORATION.
 
 # build cuspatial with verbose output
-./build.sh -v libcuspatial tests benchmarks --allgpuarch -n \
-    --cmake-args=\"-DNVBench_ENABLE_CUPTI=OFF\"
+./build.sh -v libcuspatial tests benchmarks --allgpuarch -n
