4e91076e42136900e79b6b73f25df3a40580afc5
Kyle Edwards <kyedwards@nvidia.com>
2024-03-06 17:12:40 -0500

    Remove hard-coding of RAPIDS version where possible
    Issue: https://github.com/rapidsai/build-planning/issues/15

--- ci/build_docs.sh
+++ ci/build_docs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2023, NVIDIA CORPORATION.
+# Copyright (c) 2023-2024, NVIDIA CORPORATION.
 
 set -euo pipefail
 
@@ -27,7 +27,9 @@ rapids-mamba-retry install \
   cuspatial \
   cuproj
 
-export RAPIDS_VERSION_NUMBER="24.04"
+export RAPIDS_VERSION="$(cat VERSION)"
+export RAPIDS_VERSION_MAJOR_MINOR="$(sed -E -e 's/^([0-9]{2})\.([0-9]{2})\.([0-9]{2}).*$/\1.\2/')"
+export RAPIDS_VERSION_NUMBER="$RAPIDS_VERSION_MAJOR_MINOR"
 export RAPIDS_DOCS_DIR="$(mktemp -d)"
 
 rapids-logger "Build cuSpatial CPP docs"
