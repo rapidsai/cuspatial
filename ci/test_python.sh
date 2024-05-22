7402db6d6971cea92596e62170e26365bd82255f
Michael Sarahan <msarahan@nvidia.com>
2024-05-22 17:24:34 -0400

    conda-env does not support file channels in dependencies...

--- ci/test_python.sh
+++ ci/test_python.sh
@@ -23,10 +23,6 @@ set +u
 conda activate test
 set -u
 
-rapids-logger "Downloading artifacts from previous jobs"
-CPP_CHANNEL=$(rapids-download-conda-from-s3 cpp)
-PYTHON_CHANNEL=$(rapids-download-conda-from-s3 python)
-
 RAPIDS_TESTS_DIR=${RAPIDS_TESTS_DIR:-"${PWD}/test-results"}
 RAPIDS_COVERAGE_DIR=${RAPIDS_COVERAGE_DIR:-"${PWD}/coverage-results"}
 mkdir -p "${RAPIDS_TESTS_DIR}" "${RAPIDS_COVERAGE_DIR}"
