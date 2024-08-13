e4ad129f2253f04b20e1c034b5a4f6915e9999d0
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2024-08-13 11:32:09 -0700

    Update python/cuspatial/cuspatial/core/geodataframe.py
    Co-authored-by: Mark Harris <783069+harrism@users.noreply.github.com>

--- python/cuspatial/cuspatial/core/geodataframe.py
+++ python/cuspatial/cuspatial/core/geodataframe.py
@@ -169,8 +169,10 @@ class GeoDataFrame(cudf.DataFrame):
         The output is meant for GeoDataFrame._from_data.
         """
         if not (
-            geo_columns.empty or data_columns.empty
-        ) and not geo_columns.index.equals(data_columns.index):
+            geo_columns.empty or
+            data_columns.empty or
+            geo_columns.index.equals(data_columns.index)
+        ):
             raise ValueError("geo_columns.index must equal data_columns.index")
 
         columns_mask = self.columns
