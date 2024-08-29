59f14f19b69f7b8b291839d482f7a67f5eeccccd
James Lamb <jlamb@nvidia.com>
2024-08-29 11:47:14 -0700

    update build scripts, cuspatial cmake

--- ci/test_wheel_cuspatial.sh
+++ ci/test_wheel_cuspatial.sh
@@ -10,13 +10,15 @@ RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 apt update
 DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends libgdal-dev
 
-# Download the cuspatial built in the previous step
+# Download the cuspatial and libcuspatial built in the previous step
 RAPIDS_PY_WHEEL_NAME="cuspatial_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 python ./dist
+RAPIDS_PY_WHEEL_NAME="libcuspatial_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 cpp ./dist
 
 # echo to expand wildcard before adding `[extra]` requires for pip
 python -m pip install \
   --no-binary 'fiona' \
   "$(echo ./dist/cuspatial*.whl)[test]" \
+  "$(echo ./dist/libcuspatial*.whl)" \
   'fiona>=1.8.19,<1.9'
 
 rapids-logger "pytest cuspatial"
--- python/libcuspatial/libcuspatial/load.py
+++ python/libcuspatial/libcuspatial/load.py
@@ -20,23 +20,26 @@ import libcudf
 
 
 def load_library():
-    # libcudf must be loaded before libcuspatial because libcuspatial references its symbols
+    # libcudf must be loaded before libcuspatial because libcuspatial
+    # references its symbols
     libcudf.load_library()
 
-    # Dynamically load libcudf.so. Prefer a system library if one is present to
-    # avoid clobbering symbols that other packages might expect, but if no
-    # other library is present use the one in the wheel.
+    # Dynamically load libcuspatial.so. Prefer a system library if one is
+    # present to avoid clobbering symbols that other packages might expect,
+    # but if no other library is present use the one in the wheel.
     libcuspatial_lib = None
     try:
         libcuspatial_lib = ctypes.CDLL("libcuspatial.so", ctypes.RTLD_GLOBAL)
     except OSError:
-        # If neither of these directories contain the library, we assume we are in an
-        # environment where the C++ library is already installed somewhere else and the
-        # CMake build of the libcudf Python package was a no-op.
+        # If neither of these directories contain the library, we assume we are
+        # in an environment where the C++ library is already installed
+        # somewhere else and the CMake build of the libcuspatial Python package
+        # was a no-op.
         #
-        # Note that this approach won't work for real editable installs of the libcudf package.
-        # scikit-build-core has limited support for importlib.resources so there isn't a clean
-        # way to support that case yet.
+        # Note that this approach won't work for real editable installs of the
+        # libcuspatial package. scikit-build-core has limited support for
+        # importlib.resources so there isn't a clean way to support that case
+        # yet.
         for lib_dir in ("lib", "lib64"):
             if os.path.isfile(
                 lib := os.path.join(
@@ -48,5 +51,5 @@ def load_library():
 
     # The caller almost never needs to do anything with this library, but no
     # harm in offering the option since this object at least provides a handle
-    # to inspect where libcudf was loaded from.
+    # to inspect where the library was loaded from.
     return libcuspatial_lib
