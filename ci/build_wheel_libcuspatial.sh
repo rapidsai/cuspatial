f59a0678d788f3592976cab660af62f1ec2d0932
James Lamb <jlamb@nvidia.com>
2024-10-17 10:37:18 -0500

    test with new rapids-dependency-file-generator

--- ci/build_wheel_libcuspatial.sh
+++ ci/build_wheel_libcuspatial.sh
@@ -3,20 +3,23 @@
 
 set -euo pipefail
 
+git clone \
+  --branch multiple-file-keys \
+  https://github.com/jameslamb/dependency-file-generator.git \
+  /tmp/rapids-dependency-file-generator
+
+pip uninstall --yes rapids-dependency-file-generator
+pip install /tmp/rapids-dependency-file-generator
+
 rapids-logger "Generating build requirements"
 matrix_selectors="cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch);py=${RAPIDS_PY_VERSION};cuda_suffixed=true"
 
 rapids-dependency-file-generator \
   --output requirements \
   --file-key "py_build_${package_name}" \
-  --matrix "${matrix_selectors}" \
-| tee /tmp/requirements-build.txt
-
-rapids-dependency-file-generator \
-  --output requirements \
   --file-key "py_rapids_build_${package_name}" \
   --matrix "${matrix_selectors}" \
-| tee -a /tmp/requirements-build.txt
+| tee /tmp/requirements-build.txt
 
 rapids-logger "Installing build requirements"
 python -m pip install \
