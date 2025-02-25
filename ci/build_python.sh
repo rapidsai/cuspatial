262b3fb64ac619d1185aecd950b00df24ded59db
Bradley Dice <bdice@bradleydice.com>
2025-02-25 15:04:58 -0600

    Use conda-build instead of conda-mambabuild (#1544)
    This changes from `conda mambabuild` to `conda build`. Conda now uses the mamba solver so no performance regressions are expected.
    
    This is a temporary change as we plan to migrate to `rattler-build` in the near future. However, this is needed sooner to drop `boa` and unblock Python 3.13 migrations.
    
    xref: https://github.com/rapidsai/build-planning/issues/149
    
    Authors:
      - Bradley Dice (https://github.com/bdice)
    
    Approvers:
      - James Lamb (https://github.com/jameslamb)
    
    URL: https://github.com/rapidsai/cuspatial/pull/1544

--- ci/build_python.sh
+++ ci/build_python.sh
@@ -23,7 +23,7 @@ sccache --zero-stats
 
 # TODO: Remove `--no-test` flag once importing on a CPU
 # node works correctly
-RAPIDS_PACKAGE_VERSION=$(head -1 ./VERSION) rapids-conda-retry mambabuild \
+RAPIDS_PACKAGE_VERSION=$(head -1 ./VERSION) rapids-conda-retry build \
   --no-test \
   --channel "${CPP_CHANNEL}" \
   conda/recipes/cuspatial
@@ -36,7 +36,7 @@ sccache --zero-stats
 
 # TODO: Remove `--no-test` flag once importing on a CPU
 # node works correctly
-RAPIDS_PACKAGE_VERSION=$(head -1 ./VERSION) rapids-conda-retry mambabuild \
+RAPIDS_PACKAGE_VERSION=$(head -1 ./VERSION) rapids-conda-retry build \
   --no-test \
   --channel "${CPP_CHANNEL}" \
   conda/recipes/cuproj
