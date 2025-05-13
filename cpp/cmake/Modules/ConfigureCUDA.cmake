3c397d299e29e172447b5d832a48f0be1d0d5e8b
Robert Maynard <rmaynard@nvidia.com>
2025-05-13 13:56:37 -0400

    CUDA 12.9 correctly bound compression flag

--- cpp/cmake/Modules/ConfigureCUDA.cmake
+++ cpp/cmake/Modules/ConfigureCUDA.cmake
@@ -32,7 +32,10 @@ list(APPEND CUSPATIAL_CUDA_FLAGS -Xcompiler=-Wall,-Werror,-Wno-error=deprecated-
 
 # Produce smallest binary size
 list(APPEND CUSPATIAL_CUDA_FLAGS -Xfatbin=-compress-all)
-if(CMAKE_CUDA_COMPILER_ID STREQUAL "NVIDIA" AND CMAKE_CUDA_COMPILER_VERSION VERSION_EQUAL 12.9)
+if(CMAKE_CUDA_COMPILER_ID STREQUAL "NVIDIA"
+   AND (CMAKE_CUDA_COMPILER_VERSION VERSION_GREATER_EQUAL 12.9 AND CMAKE_CUDA_COMPILER_VERSION
+                                                                   VERSION_LESS 13.0)
+)
   list(APPEND CUSPATIAL_CUDA_FLAGS -Xfatbin=--compress-level=3)
 endif()
 
